99e46dbb6c51d3b44a490e2963cdbca3a65b7213
Daniel Vicarel <shundra8820@gmail.com>
2016-09-14 02:13:23 -0400

    Added a component to mark GameObjects as DontDestroyOnLoad()

---
+++ Danware.Unity/DontDestroy.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace Danware.Unity {
+
+    public class DontDestroy : MonoBehaviour {
+
+        private void Awake() {
+            DontDestroyOnLoad(gameObject);
+        }
+
+    }
+
+}
