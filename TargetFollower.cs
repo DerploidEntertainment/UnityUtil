a48793d8b7b9de52d6a00e74f2fbd25f2e015640
Daniel Vicarel <shundra8820@gmail.com>
2017-03-26 12:40:27 -0400

    TargetFollower can now make an ARRAY of followers follow the target with the given offset

--- TargetFollower.cs
+++ TargetFollower.cs
@@ -4,16 +4,17 @@ namespace Danware.Unity {
 
     public class TargetFollower : MonoBehaviour {
         // INSPECTOR FIELDS
-        public Transform Follower;
+        [Tooltip("An array of GameObjects that will all follow the Target at the given Offset")]
+        public Transform[] Followers;
+        [Tooltip("The Target being followed by all Followers at the given Offset")]
         public Transform Target;
+        [Tooltip("The Offset at which all Followers will follow the Target")]
         public Vector3 Offset = new Vector3(0f, 0f, -10f);
 
         // EVENT HANDLERS
-        private void Awake() {
-
-        }
         private void Update() {
-            Follower.transform.position = Target.position + Offset;
+            for (int f = 0; f < Followers.Length; ++f)
+                Followers[f].transform.position = Target.position + Offset;
         }
     }
 
