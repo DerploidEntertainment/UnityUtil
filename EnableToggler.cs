3c2a689299c90c2984a7985c74dffb48f5ff1b15
Daniel Vicarel <shundra8820@gmail.com>
2017-04-29 23:56:04 -0400

    Added ActiveToggler and EnableToggler components that behave similarly to Destroyer

---
+++ EnableToggler.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace Danware.Unity {
+
+    public class EnableToggler : MonoBehaviour {
+
+        // INSPECTOR FIELDS
+        public MonoBehaviour ComponentToToggle;
+
+        // API INTERFACE
+        public void EnableComponent() {
+            ComponentToToggle.enabled = true;
+        }
+        public void DisableComponent() {
+            ComponentToToggle.enabled = false;
+        }
+
+    }
+
+}
