bb9c069188eef07adf7454c0c17931ad60cef7e8
Daniel Vicarel <shundra8820@gmail.com>
2017-08-20 04:55:08 -0400

    Replaced AnimationEventDetector with a more intuitive AnimationEventTrigger

---
+++ Triggers/AnimationEventTrigger.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+namespace Danware.Unity {
+
+    [RequireComponent(typeof(Animator))]
+    public class AnimationEventTrigger : MonoBehaviour {
+
+        public class AnimationEventTriggerEvent : UnityEvent<Animator, string> { }
+
+        // HIDDEN FIELDS
+        private Animator _animator;
+
+        // EVENT HANDLERS
+        private void Awake() => _animator = GetComponent<Animator>();
+
+        // API INTERFACE
+        public AnimationEventTriggerEvent AnimationEventOccurred = new AnimationEventTriggerEvent();
+        /// <summary>
+        /// Warning! This method must not be called programmatically.  Instead, create an <see cref="AnimationClip"/> with an <see cref="AnimationEvent"/> that calls this method.
+        /// </summary>
+        /// <param name="eventName">Name of the event that was raised by the <see cref="Animator"/></param>
+        public void RaiseEvent(string eventName) => AnimationEventOccurred.Invoke(_animator, eventName);
+
+    }
+
+}
