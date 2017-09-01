5e5eb9af278e98ddf8b20fc9e2c02b7dca232239
Dan Vicarel <Daniel.Vicarel@SupplyTechnologies.com>
2017-09-01 18:20:14 -0400

    Added a RandomTrigger that will trigger a Random trigger when triggered (say that 5 times fast)

---
+++ Triggers/RandomTrigger.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace Danware.Unity.Triggers {
+
+    public class RandomTrigger : MonoBehaviour {
+
+        public SimpleTrigger[] Triggers;
+
+        public void Trigger() {
+            int t = Random.Range(0, Triggers.Length);
+            Triggers[t].Trigger();
+        }
+
+    }
+
+}
