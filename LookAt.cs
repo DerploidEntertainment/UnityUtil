1c4a103b77aad3ea4a40edcd3f391884f638518f
Daniel Vicarel <shundra8820@gmail.com>
2017-10-11 00:35:43 -0400

    Added a LookAt component to keep a Transform always rotated toward another Transform

---
+++ LookAt.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace Danware.Unity {
+
+    public class LookAt : MonoBehaviour {
+
+        public Transform TransformToRotate;
+        public Transform TransformToLookAt;
+        public bool FlipOnLocalY = false;
+
+        private void Awake() {
+            Assert.IsNotNull(TransformToRotate, $"{GetType().Name} {transform.parent.name}.{name} must be associated with a {nameof(this.TransformToRotate)}!");
+            Assert.IsNotNull(TransformToLookAt, $"{GetType().Name} {transform.parent.name}.{name} must be associated with a {nameof(this.TransformToLookAt)}!");
+        }
+        private void Update() {
+            TransformToRotate.LookAt(TransformToLookAt, -Physics.gravity);
+            if (FlipOnLocalY)
+                TransformToRotate.localRotation *= Quaternion.Euler(180f * Vector3.up);
+        }
+
+    }
+
+}
