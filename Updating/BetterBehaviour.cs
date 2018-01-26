32165b83f6e03289d2d6e33008edbc75185f10c6
Daniel Vicarel <shundra8820@gmail.com>
2018-01-26 02:45:30 -0500

    Added a BetterBehaviour class to remove some of the boilerplate of registering update actions (I know, f*ck new base classes...)

---
+++ Updating/BetterBehaviour.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using UnityEngine;
+
+namespace UnityUtil {
+
+    public abstract class BetterBehaviour : MonoBehaviour {
+
+        protected int _instanceID;
+
+        protected Action _betterUpdate;
+        protected Action _betterFixedUpdate;
+        protected Action _betterLateUpdate;
+
+        [Inject]
+        public UpdaterSingleton Updater;
+
+        protected void Awake() {
+            _instanceID = GetInstanceID();
+
+            BetterAwake();
+        }
+        protected void OnEnable() {
+            if  (_betterUpdate != null)
+                Updater.RegisterUpdate(_instanceID, _betterUpdate);
+            if (_betterFixedUpdate != null)
+                Updater.RegisterFixedUpdate(_instanceID, _betterFixedUpdate);
+            if (_betterLateUpdate != null)
+                Updater.RegisterLateUpdate(_instanceID, _betterLateUpdate);
+
+            BetterOnEnable();
+        }
+        protected void OnDisable() {
+            if (_betterUpdate != null)
+                Updater.UnregisterUpdate(_instanceID);
+            if (_betterFixedUpdate != null)
+                Updater.UnregisterFixedUpdate(_instanceID);
+            if (_betterLateUpdate != null)
+                Updater.UnregisterLateUpdate(_instanceID);
+
+            BetterOnDisable();
+        }
+
+        protected virtual void BetterAwake() { }
+        protected virtual void BetterOnEnable() { }
+        protected virtual void BetterOnDisable() { }
+
+    }
+
+}
