39462b8a37b4a41eb9f0e27a29502926303ad3a4
Daniel Vicarel <shundra8820@gmail.com>
2018-02-12 01:13:27 -0500

    BetterBehaviour now has a protected RegisterUpdatesAutomatically flag that determines whether its actions will be registered/unregistered automatically when the Component is enabled/disabled.

--- UnityUtil/Updating/BetterBehaviour.cs
+++ UnityUtil/Updating/BetterBehaviour.cs
@@ -6,11 +6,18 @@ namespace UnityUtil {
 
     public abstract class BetterBehaviour : MonoBehaviour {
 
-        protected int _instanceID;
+        // HIDDEN FIELDS
+        /// <summary>
+        /// If <see langword="true"/>, then this <see cref="UnityUtil.BetterBehaviour"/> will have its Update actions registered/unregistered automatically when it is enabled/disabled.
+        /// If <see langword="false"/>, then the Update actions must be registered/unregistered manually (best for when updates are only meant to be registered under specific/rare circumstances).
+        /// <summary>
+        protected bool RegisterUpdatesAutomatically = true;
 
-        protected Action _betterUpdate;
-        protected Action _betterFixedUpdate;
-        protected Action _betterLateUpdate;
+        protected int InstanceID;
+
+        protected Action BetterUpdate;
+        protected Action BetterFixedUpdate;
+        protected Action BetterLateUpdate;
 
         [Inject]
         public UpdaterSingleton Updater;
@@ -18,33 +25,46 @@ namespace UnityUtil {
         protected void Awake() {
             Assert.IsNotNull(Updater, this.GetAssociationAssertion(nameof(this.Updater), singleton: true));
 
-            _instanceID = GetInstanceID();
+            InstanceID = GetInstanceID();
 
             BetterAwake();
         }
         protected void OnEnable() {
-            if  (_betterUpdate != null)
-                Updater.RegisterUpdate(_instanceID, _betterUpdate);
-            if (_betterFixedUpdate != null)
-                Updater.RegisterFixedUpdate(_instanceID, _betterFixedUpdate);
-            if (_betterLateUpdate != null)
-                Updater.RegisterLateUpdate(_instanceID, _betterLateUpdate);
+            if (RegisterUpdatesAutomatically) {
+                if (BetterUpdate != null)
+                    Updater.RegisterUpdate(InstanceID, BetterUpdate);
+                if (BetterFixedUpdate != null)
+                    Updater.RegisterFixedUpdate(InstanceID, BetterFixedUpdate);
+                if (BetterLateUpdate != null)
+                    Updater.RegisterLateUpdate(InstanceID, BetterLateUpdate);
+            }
 
             BetterOnEnable();
         }
         protected void OnDisable() {
-            if (_betterUpdate != null)
-                Updater.UnregisterUpdate(_instanceID);
-            if (_betterFixedUpdate != null)
-                Updater.UnregisterFixedUpdate(_instanceID);
-            if (_betterLateUpdate != null)
-                Updater.UnregisterLateUpdate(_instanceID);
+            if (RegisterUpdatesAutomatically) {
+                if (BetterUpdate != null)
+                    Updater.UnregisterUpdate(InstanceID);
+                if (BetterFixedUpdate != null)
+                    Updater.UnregisterFixedUpdate(InstanceID);
+                if (BetterLateUpdate != null)
+                    Updater.UnregisterLateUpdate(InstanceID);
+            }
 
             BetterOnDisable();
         }
 
+        /// <summary>
+        /// Actions to run during Awake().  Declaring Awake() on a subclass would hide the implementation in <see cref="UnityUtil.BetterBehaviour"/>, so this method was provided for subclasses to provide additional Awake() functionality.
+        /// </summary>
         protected virtual void BetterAwake() { }
+        /// <summary>
+        /// Actions to run during OnEnable().  Declaring OnEnable() on a subclass would hide the implementation in <see cref="UnityUtil.BetterBehaviour"/>, so this method was provided for subclasses to provide additional OnEnable() functionality.
+        /// </summary>
         protected virtual void BetterOnEnable() { }
+        /// <summary>
+        /// Actions to run during OnDisable().  Declaring OnDisable() on a subclass would hide the implementation in <see cref="UnityUtil.BetterBehaviour"/>, so this method was provided for subclasses to provide additional OnDisable() functionality.
+        /// </summary>
         protected virtual void BetterOnDisable() { }
 
     }
