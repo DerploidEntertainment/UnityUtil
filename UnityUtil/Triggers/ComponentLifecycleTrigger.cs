d0995d83e4d9bec0bbe9df677b67bf0e26a73bcb
Daniel Vicarel <shundra8820@gmail.com>
2018-02-27 02:12:31 -0500

    Added a ComponentLifecycleTrigger

---
+++ UnityUtil/Triggers/ComponentLifecycleTrigger.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+namespace UnityUtil.Triggers {
+
+    public class ComponentLifecycleTrigger : MonoBehaviour {
+
+        // INSPECTOR FIELDS
+        public UnityEvent Awoken = new UnityEvent();
+        public UnityEvent Started = new UnityEvent();
+        public UnityEvent Enabled = new UnityEvent();
+        public UnityEvent Disabled = new UnityEvent();
+        public UnityEvent Destroyed = new UnityEvent();
+
+        // EVENT HANDLERS
+        private void Awake() => Awoken.Invoke();
+        private void Start() => Started.Invoke();
+        private void OnEnable() => Enabled.Invoke();
+        private void OnDisable() => Disabled.Invoke();
+        private void OnDestroy() => Destroyed.Invoke();
+
+    }
+
+}
