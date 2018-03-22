301e79ffe73e3784c62cdf2f09325d072b56fc2a
Daniel Vicarel <shundra8820@gmail.com>
2018-03-21 21:05:55 -0400

    Added two new LookAtInteractor/2D components that "interact with" ToggleTriggers

---
+++ UnityUtil/Input/Interaction/LookAtInteractor.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityUtil.Triggers;
+
+namespace UnityUtil.Input {
+
+    public class LookAtInteractor : BetterBehaviour {
+
+        private ToggleTrigger _trigger;
+
+        public float Range;
+        public LayerMask InteractLayerMask;
+
+        protected override void BetterAwake() {
+            RegisterUpdatesAutomatically = true;
+            BetterUpdate = look;
+        }
+
+        private void look() {
+            bool somethingHit = Physics.Raycast(transform.position, transform.forward, out RaycastHit hitInfo, Range, InteractLayerMask);
+            if (somethingHit) {
+                ToggleTrigger trigger = hitInfo.collider.GetComponent<ToggleTrigger>();
+                if (_trigger == null) {
+                    _trigger = trigger;
+                    _trigger.TurnOn();
+                }
+                else if (trigger != _trigger) {
+                    _trigger.TurnOff();
+                    _trigger = trigger;
+                    _trigger.TurnOn();
+                }
+            }
+            else
+                _trigger?.TurnOff();
+        }
+
+    }
+
+}
---
+++ UnityUtil/Input/Interaction/LookAtInteractor2D.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityUtil.Triggers;
+
+namespace UnityUtil.Input {
+
+    public class LookAtInteractor2D : BetterBehaviour {
+
+        private ToggleTrigger _trigger;
+
+        public float Range;
+        public LayerMask InteractLayerMask;
+
+        protected override void BetterAwake() {
+            RegisterUpdatesAutomatically = true;
+            BetterUpdate = look;
+        }
+
+        private void look() {
+            RaycastHit2D hit = Physics2D.Raycast(transform.position, transform.forward, Range, InteractLayerMask);
+            ToggleTrigger trigger = hit.collider?.GetComponent<ToggleTrigger>();
+            if (trigger == null)
+                _trigger?.TurnOff();
+            else {
+                if (_trigger == null) {
+                    _trigger = trigger;
+                    _trigger.TurnOn();
+                }
+                else if (trigger != _trigger) {
+                    _trigger.TurnOff();
+                    _trigger = trigger;
+                    _trigger.TurnOn();
+                }
+            }
+        }
+
+    }
+
+}
