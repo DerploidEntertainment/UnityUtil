fe7f3e671b182dc712da4f22ebd028cb8a6e268e
Daniel Vicarel <shundra8820@gmail.com>
2018-03-21 21:03:36 -0400

    The LookAt component now derives from BetterBehaviour

--- UnityUtil/Movement/LookAt.cs
+++ UnityUtil/Movement/LookAt.cs
@@ -3,17 +3,20 @@ using UnityEngine.Assertions;
 
 namespace UnityUtil {
 
-    public class LookAt : MonoBehaviour {
+    public class LookAt : BetterBehaviour {
 
         public Transform TransformToRotate;
         public Transform TransformToLookAt;
         public bool FlipOnLocalY = false;
 
-        private void Awake() {
+        protected override void BetterAwake() {
             Assert.IsNotNull(TransformToRotate, this.GetAssociationAssertion(nameof(this.TransformToRotate)));
             Assert.IsNotNull(TransformToLookAt, this.GetAssociationAssertion(nameof(this.TransformToLookAt)));
+
+            RegisterUpdatesAutomatically = true;
+            BetterUpdate = look;
         }
-        private void Update() {
+        private void look() {
             TransformToRotate.LookAt(TransformToLookAt, -Physics.gravity);
             if (FlipOnLocalY)
                 TransformToRotate.localRotation *= Quaternion.Euler(180f * Vector3.up);
