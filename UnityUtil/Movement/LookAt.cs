9c98f6f40e9c0fb8e2f1a70e34f399fad06df5d4
Dan Vicarel <Daniel.Vicarel@supplytechnologies.com>
2018-03-27 12:54:44 -0400

    Removed some assertions from the LookAt component.  It will now check that TransformToRotate and TransformToLookAt are both non-null during Update

--- UnityUtil/Movement/LookAt.cs
+++ UnityUtil/Movement/LookAt.cs
@@ -10,13 +10,13 @@ namespace UnityUtil {
         public bool FlipOnLocalY = false;
 
         protected override void BetterAwake() {
-            Assert.IsNotNull(TransformToRotate, this.GetAssociationAssertion(nameof(this.TransformToRotate)));
-            Assert.IsNotNull(TransformToLookAt, this.GetAssociationAssertion(nameof(this.TransformToLookAt)));
-
             RegisterUpdatesAutomatically = true;
             BetterUpdate = look;
         }
         private void look() {
+            if (TransformToRotate == null || TransformToLookAt == null)
+                return;
+
             TransformToRotate.LookAt(TransformToLookAt, -Physics.gravity);
             if (FlipOnLocalY)
                 TransformToRotate.localRotation *= Quaternion.Euler(180f * Vector3.up);
