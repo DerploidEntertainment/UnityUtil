df963c4b4d6f7d8187e795fbcef98115c8a23dc8
Dan Vicarel <Daniel.Vicarel@supplytechnologies.com>
2018-03-27 18:10:09 -0400

    WIP: added a component to adjust what LookAt components are looking at

---
+++ UnityUtil/Movement/LookAtAdjuster.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace UnityUtil {
+
+    /// <summary>
+    /// Tells all associated <see cref="UnityUtil.LookAt"/> components when to look at a new <see cref="Transform"/>.
+    /// </summary>
+    public class LookAtAdjuster : BetterBehaviour {
+
+        [Tooltip("These are the " + nameof(UnityUtil.LookAt) + " components that will be told what new Transforms to look at.")]
+        public LookAt[] AssociatedLookAts;
+
+        /// <summary>
+        /// You can actually set <see cref="LookAt.TransformToLookAt"/> directly.  This function was only created for use with UnityEvents.
+        /// </summary>
+        /// <param name="transform">The new <see cref="Transform"/> to make the <see cref="LookAt.TransformToRotate"/> look at.</param>
+        public void SetTransformToLookAt(Transform transform) {
+            for (int la = 0; la < AssociatedLookAts.Length; ++la)
+                AssociatedLookAts[la].TransformToLookAt = transform;
+        }
+
+    }
+
+}
