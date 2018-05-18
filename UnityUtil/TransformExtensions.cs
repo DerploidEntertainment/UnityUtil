d6b23502d8ab3a2d057d22bb33230e86701de353
Dan Vicarel <shundra8820@gmail.com>
2018-05-18 03:07:44 -0400

    Added a TransformExtensions static class for Transform extension methods

---
+++ UnityUtil/TransformExtensions.cs
@@ -0,0 +1,17 @@
+﻿namespace UnityEngine {
+
+    public static class TransformExtensions {
+
+        public static bool HasParent(this Transform transform, Transform parent, int generationLimit = -1) {
+            Transform pTrans;
+            int genCount = 0;
+            do {
+                ++genCount;
+                pTrans = transform.parent;
+            } while (pTrans != parent && pTrans != null && genCount < generationLimit);
+            return pTrans = parent;
+        }
+
+    }
+
+}
