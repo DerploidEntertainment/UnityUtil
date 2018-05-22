63acadf89db75c48292f8d7a9f772d96bf59fd73
Dan Vicarel <shundra8820@gmail.com>
2018-05-22 02:48:50 -0400

    TargetFlyer now has an "idle" state where it will just slowly, randomly, spin and move about.  (Should probably eventually be its own component...)

--- UnityUtil/Movement/TargetFlyer.cs
+++ UnityUtil/Movement/TargetFlyer.cs
@@ -1,54 +1,120 @@
-﻿using UnityEngine;
+﻿namespace UnityEngine.Movement {
 
-namespace UnityEngine.Movement {
+    public class TargetFlyer : Updatable {
 
-    public class TargetFlyer : MonoBehaviour {
+        // HIDDEN FIELDS
+        private float _tSinceIdleMove = 0f;
+        private float _tSinceIdleTorque = 0f;
+        private float _idleMovePeriod = 0f;
+        private float _idleTorquePeriod = 0f;
+
         // INSPECTOR FIELDS
+        public bool Idle = false;
         public Rigidbody FlyingRigidbody;
+
+        [Header("Flying Settings")]
+        public Vector3 Target;
         public float MoveSpeed = 20f;
         public float MoveAccel = 35f;
         public bool RotateClockWise = true;
         public float RotateSpeed = 5f;
         public float RotateAccel = 5f;
-        public Vector3 Target;
 
-        // UNITY FUNCTIONS
-        private void FixedUpdate() {
+        [Header("Idle Settings")]
+        public float MinMovePeriod = 0.5f;
+        public float MaxMovePeriod = 2f;
+        public float MinMoveSpeed = 0.5f;
+        public float MaxMoveSpeed = 2f;
+        public float MinTorquePeriod = 0.5f;
+        public float MaxTorquePeriod = 2f;
+        public float MinTorqueSpeed = 0.5f;
+        public float MaxTorqueSpeed = 2f;
+
+        // EVENT HANDLERS
+        protected override void BetterAwake() {
+            // Set initial idle move/torque periods for if/when we idle
+            _idleMovePeriod = Random.Range(MinMovePeriod, MaxMovePeriod);
+            _idleTorquePeriod = Random.Range(MinTorquePeriod, MaxTorquePeriod);
+
+            RegisterUpdatesAutomatically = true;
+            BetterFixedUpdate = flyOrIdle;
+        }
+        private void flyOrIdle() {
+            if (FlyingRigidbody == null)
+                return;
+
+            // If idling, then just apply an occasional, random force
+            if (Idle) {
+                FlyingRigidbody.AddForce(getIdleForce(), ForceMode.VelocityChange);
+                FlyingRigidbody.AddTorque(getIdleTorque(), ForceMode.VelocityChange);
+            }
+
             // Move/rotate towards the target vector
-            if (FlyingRigidbody != null) {
-                flyRotate(Target);
-                flyMove(Target);
+            else {
+                FlyingRigidbody.AddForce(getFlyingForce(Target), ForceMode.Acceleration);
+                FlyingRigidbody.AddTorque(getFlyingTorque(Target), ForceMode.Acceleration);
             }
         }
 
-        // HELPER FUNCTIONS
-        private void flyMove(Vector3 targetPosition) {
+        // HELPERS
+        private Vector3 getFlyingForce(Vector3 targetPosition) {
+            Vector3 netForce = Vector3.zero;
+
             // Add a Force to move towards the target position at constant velocity
             Vector3 toward = (targetPosition - transform.position).normalized;
             var vToward = Vector3.Project(FlyingRigidbody.velocity, toward);
             float factor = (vToward.normalized == toward) ? Mathf.Sign(MoveSpeed * MoveSpeed - vToward.sqrMagnitude) : 1;
-            FlyingRigidbody.AddForce(factor * MoveAccel * toward, ForceMode.Acceleration);
+            netForce += factor * MoveAccel * toward;
 
             // Add a Force to reduce any velocity in the normal direction
             var vNorm = Vector3.ProjectOnPlane(FlyingRigidbody.velocity, toward);
             Vector3 norm = vNorm.normalized;
             if (vNorm.sqrMagnitude > 0f)
-                FlyingRigidbody.AddForce(-MoveAccel * norm, ForceMode.Acceleration);
+                netForce -= MoveAccel * norm;
+
+            return netForce;
         }
-        private void flyRotate(Vector3 targetPosition) {
+        private Vector3 getFlyingTorque(Vector3 targetPosition) {
+            Vector3 netTorque = Vector3.zero;
+
             // Add a Force to rotate around the target direction at constant angular velocity
             Vector3 toward = (targetPosition - transform.position).normalized;
             var wToward = Vector3.Project(FlyingRigidbody.angularVelocity, toward);
             float factor = (wToward.normalized == toward) ? Mathf.Sign(RotateSpeed * RotateSpeed - wToward.sqrMagnitude) : 1;
             factor *= (RotateClockWise ? 1 : -1);
-            FlyingRigidbody.AddTorque(factor * RotateAccel * toward, ForceMode.Acceleration);
+            netTorque += factor * RotateAccel * toward;
 
             // Add a Force to reduce any angular velocity around the normal direction
             var wNorm = Vector3.ProjectOnPlane(FlyingRigidbody.angularVelocity, toward);
             Vector3 norm = wNorm.normalized;
             if (wNorm.sqrMagnitude > 0f)
-                FlyingRigidbody.AddTorque(-RotateAccel * norm, ForceMode.Acceleration);
+                netTorque -= RotateAccel * norm;
+
+            return netTorque;
         }
+        private Vector3 getIdleForce() {
+            _tSinceIdleMove += Time.deltaTime;
+            if (_tSinceIdleMove>= _idleMovePeriod) {
+                _tSinceIdleMove -= _idleMovePeriod;
+                _idleMovePeriod = Random.Range(MinMovePeriod, MaxMovePeriod);
+                float mag = Random.Range(MinMoveSpeed, MaxMoveSpeed);
+                return mag * Random.onUnitSphere;
+            }
+
+            return Vector3.zero;
+        }
+        private Vector3 getIdleTorque() {
+            _tSinceIdleTorque += Time.deltaTime;
+            if (_tSinceIdleTorque >= _idleTorquePeriod) {
+                _tSinceIdleTorque -= _idleTorquePeriod;
+                _idleTorquePeriod = Random.Range(MinTorquePeriod, MaxTorquePeriod);
+                float mag = Random.Range(MinTorqueSpeed, MaxTorqueSpeed);
+                return mag * Random.onUnitSphere;
+            }
+
+            return Vector3.zero;
+        }
+
     }
 
 }
