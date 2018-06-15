6dca6d6e13e53d3c49a817f00a8d8d4ff0f11dfd
Dan Vicarel <shundra8820@gmail.com>
2018-06-15 00:50:23 -0400

    Weapons now have a "PhysicsCastShape" enum field, allowing the shape of their area-of-effect to be adjusted

--- UnityUtil/Inventory/Weapon.cs
+++ UnityUtil/Inventory/Weapon.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using UnityEngine.Assertions;
 using UnityEngine.Events;
@@ -32,11 +33,28 @@ namespace UnityEngine.Inventory {
             _tool.Using.AddListener(() => _accuracyLerpT = 0f);
             _tool.Used.AddListener(attack);
         }
-        private void OnDrawGizmos() =>
-            Gizmos.DrawLine(transform.position, transform.position + Info.Range * transform.forward);
+        private void OnDrawGizmos() {
+            switch (Info.PhysicsCastShape) {
+                case PhysicsCastShape.Ray:
+                case PhysicsCastShape.Capsule:  // No Gizmos.DrawCapsule method unfortunately :/
+                    Gizmos.DrawLine(transform.position, transform.position + Info.Range * transform.forward);
+                    break;
+
+                case PhysicsCastShape.Box:
+                    Gizmos.DrawWireCube(transform.position + Info.Range * transform.forward, 2f * Info.HalfExtents);
+                    break;
+
+                case PhysicsCastShape.Sphere:
+                    Gizmos.DrawWireSphere(transform.position + Info.Range * transform.forward, Info.Radius);
+                    break;
+
+                default: this.LogWarning(" could not draw Gizmos.  " + BetterLogger.GetSwitchDefault(Info.PhysicsCastShape)); break;
+            }
+        }
 
         // HELPERS
         private void attack() {
+
             // Get a random Ray within the accuracy cone
             float z = U.Random.Range(Mathf.Cos(Mathf.Deg2Rad * AccuracyConeHalfAngle), 1f);
             float theta = U.Random.Range(0f, 2 * Mathf.PI);
@@ -44,23 +62,91 @@ namespace UnityEngine.Inventory {
             var dir = new Vector3(sqrtPart * Mathf.Cos(theta), sqrtPart * Mathf.Sin(theta), z);
             var ray = new Ray(transform.position, transform.TransformDirection(dir));
 
-            // Raycast into the scene with the given LayerMask
-            // Raise the Attacked event, allowing other components to select which components to affect
+            // Cast into the scene for hits along this ray, using the specified cast shape
             var hits = new RaycastHit[0];
-            if (Info.AttackAllInRange || Info.MaxAttacks > 1) {
-                RaycastHit[] allHits = Physics.RaycastAll(ray, Info.Range, Info.AttackLayerMask);
-                hits = allHits;
+            switch (Info.PhysicsCastShape) {
+                case PhysicsCastShape.Ray:     hits = rayAttackHits();     break;
+                case PhysicsCastShape.Box:     hits = boxAttackHits();     break;
+                case PhysicsCastShape.Sphere:  hits = sphereAttackHits();  break;
+                case PhysicsCastShape.Capsule: hits = capsuleAttackHits(); break;
+                default: throw new NotImplementedException(BetterLogger.GetSwitchDefault(Info.PhysicsCastShape));
             }
-            else if (Info.MaxAttacks == 1) {
-                bool somethingHit = Physics.Raycast(ray, out RaycastHit hit, Info.Range, Info.AttackLayerMask);
-                if (somethingHit)
-                    hits = new[] { hit };
-            }
-            hits = hits.OrderBy(h => h.distance).Take((int)Info.MaxAttacks).ToArray();
+
+            // Sort hits by increasing distance, and raise the Attacked event so that other components can select which components to affect
+            IEnumerable<RaycastHit> orderedHits = hits.OrderBy(h => h.distance);
+            if (!Info.AttackAllInRange)
+                orderedHits = orderedHits.Take((int)Info.MaxAttacks);
+            hits = orderedHits.ToArray();
             Attacked.Invoke(ray, hits);
 
             // Adjust accuracy for the next attack, assuming the base Tool is automatic
             _accuracyLerpT = (Info.AccuracyLerpTime == 0 ? 1f : _accuracyLerpT + (1f / _tool.Info.AutomaticUseRate) / Info.AccuracyLerpTime);
+            RaycastHit[] rayAttackHits() {
+                var rayHits = new RaycastHit[0];
+
+                // Raycast into the scene with the given LayerMask, collecting the desired number of hitInfos
+                if (Info.AttackAllInRange || Info.MaxAttacks > 1) {
+                    RaycastHit[] allHits = Physics.RaycastAll(ray.origin, ray.direction, Info.Range, Info.AttackLayerMask);
+                    rayHits = allHits;
+                }
+                else if (Info.MaxAttacks == 1) {
+                    bool somethingHit = Physics.Raycast(ray.origin, ray.direction, out RaycastHit hit, Info.Range, Info.AttackLayerMask);
+                    if (somethingHit)
+                        rayHits = new[] { hit };
+                }
+
+                return rayHits;
+            }
+            RaycastHit[] boxAttackHits() {
+                var boxHits = new RaycastHit[0];
+
+                // Boxcast into the scene with the given LayerMask, collecting the desired number of hitInfos
+                if (Info.AttackAllInRange || Info.MaxAttacks > 1) {
+                    RaycastHit[] allHits = Physics.BoxCastAll(ray.origin, Info.HalfExtents, ray.direction, Info.Orientation, Info.Range, Info.AttackLayerMask);
+                    boxHits = allHits;
+                }
+                else if (Info.MaxAttacks == 1) {
+                    bool somethingHit = Physics.BoxCast(ray.origin, Info.HalfExtents, ray.direction, out RaycastHit hit, Info.Orientation, Info.Range, Info.AttackLayerMask);
+                    if (somethingHit)
+                        boxHits = new[] { hit };
+                }
+
+                return boxHits;
+            }
+            RaycastHit[] sphereAttackHits() {
+                var sphereHits = new RaycastHit[0];
+
+                // Spherecast into the scene with the given LayerMask, collecting the desired number of hitInfos
+                if (Info.AttackAllInRange || Info.MaxAttacks > 1) {
+                    RaycastHit[] allHits = Physics.SphereCastAll(ray.origin, Info.Radius, ray.direction, Info.Range, Info.AttackLayerMask);
+                    sphereHits = allHits;
+                }
+                else if (Info.MaxAttacks == 1) {
+                    bool somethingHit = Physics.SphereCast(ray.origin, Info.Radius, ray.direction, out RaycastHit hit, Info.Range, Info.AttackLayerMask);
+                    if (somethingHit)
+                        sphereHits = new[] { hit };
+                }
+
+                return sphereHits;
+            }
+            RaycastHit[] capsuleAttackHits() {
+                var capsuleHits = new RaycastHit[0];
+
+                // Capsulecast into the scene with the given LayerMask, collecting the desired number of hitInfos
+                Vector3 p1 = ray.origin + Info.Point1;
+                Vector3 p2 = ray.origin + Info.Point2;
+                if (Info.AttackAllInRange || Info.MaxAttacks > 1) {
+                    RaycastHit[] allHits = Physics.CapsuleCastAll(p1, p2, Info.Radius, ray.direction, Info.Range, Info.AttackLayerMask);
+                    capsuleHits = allHits;
+                }
+                else if (Info.MaxAttacks == 1) {
+                    bool somethingHit = Physics.CapsuleCast(p1, p2, Info.Radius, ray.direction, out RaycastHit hit, Info.Range, Info.AttackLayerMask);
+                    if (somethingHit)
+                        capsuleHits = new[] { hit };
+                }
+
+                return capsuleHits;
+            }
         }
 
     }
