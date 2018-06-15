3ecb6fe3d79ffd190340cc77096281dd51ac9642
Dan Vicarel <shundra8820@gmail.com>
2018-06-15 00:50:59 -0400

    PushTools can now ignore all Colliders attached to the same Rigidbody as certain other Colliders, and never push a given Rigidbody multiple times

--- UnityUtil/Inventory/PushTool.cs
+++ UnityUtil/Inventory/PushTool.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System.Collections.Generic;
+using System.Linq;
 using UnityEngine.Assertions;
 
 namespace UnityEngine.Inventory {
@@ -7,9 +8,12 @@ namespace UnityEngine.Inventory {
     public class PushTool : MonoBehaviour {
 
         private Weapon _weapon;
+        private HashSet<Rigidbody> _pushedRigidbodies = new HashSet<Rigidbody>();
 
         // INSPECTOR FIELDS
         public PushToolInfo Info;
+        [Tooltip("If true, then any colliders attached to the same Rigidbodies as one of these colldiers will NOT be affected.  You might use this field to prevent pushing of any of the colliders that make up a player's vehicle, for example.")]
+        public Collider[] IgnoreRigidbodiesAttachedTo;
 
         // EVENT HANDLERS
         private void Awake() {
@@ -19,18 +23,28 @@ namespace UnityEngine.Inventory {
             _weapon.Attacked.AddListener(push);
         }
         private void push(Ray ray, RaycastHit[] hits) {
-            // If we should only push the closest Rigidbody, then scan for the Rigidbody to push
-            // through the hit Colliders in increasing order of distance, ignoring Colliders with the specified tags
-            // Otherwise, push the Rigidbodies on all Colliders that are not ignored with one of the specified tags
+            // Determine those Rigidbodies, the attached Colliders of which cannot be pushed
+            IEnumerable<Rigidbody> unpushableRbEnum = IgnoreRigidbodiesAttachedTo.Select(c => c.attachedRigidbody).Distinct();
+            var unpushableRbs = new HashSet<Rigidbody>(unpushableRbEnum);
+
+            // If we should only push the closest Rigidbody, then scan for the Rigidbody to push, otherwise push the Rigidbodies attached to all Colliders.
+            // Ignore Colliders with the specified tags or attached to one of the specified Rigidbodies,
+            // and be sure not to push Rigidbodies multiple times.
+            _pushedRigidbodies.Clear();
             for (int h = 0; h < hits.Length; ++h) {
                 RaycastHit hit = hits[h];
-                if (!Info.IgnoreColliderTags.Contains(hit.collider.tag)) {
-                    Rigidbody rb = hit.collider.attachedRigidbody;
-                    if (rb != null) {
-                        rb.AddForceAtPosition(Info.PushForce * ray.direction, hit.point, ForceMode.Impulse);
-                        if (Info.OnlyPushClosest && hits.Length > 0)
-                            break;
-                    }
+                Rigidbody rb = hit.collider.attachedRigidbody;
+                bool push = (
+                    rb != null &&
+                    !Info.IgnoreColliderTags.Contains(hit.collider.tag) &&
+                    !unpushableRbs.Contains(rb) &&
+                    !_pushedRigidbodies.Contains(rb)
+                );
+                if (push) {
+                    _pushedRigidbodies.Add(rb);
+                    rb.AddForceAtPosition(Info.PushForce * ray.direction, hit.point, ForceMode.Impulse);
+                    if (Info.OnlyPushClosest && hits.Length > 0)
+                        break;
                 }
             }
         }
