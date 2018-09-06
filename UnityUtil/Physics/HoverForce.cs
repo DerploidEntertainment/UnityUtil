1cc03eea65b48a1aafc921b5cfc18656480b5afb
Dan Vicarel <shundra8820@gmail.com>
2018-09-05 23:42:10 -0400

    HoverForce can now optionally use AddForceAtPosition()

--- UnityUtil/Physics/HoverForce.cs
+++ UnityUtil/Physics/HoverForce.cs
@@ -5,23 +5,27 @@ namespace UnityEngine {
 
     public class HoverForce : MonoBehaviour {
 
+        private RaycastHit _lastGroundHit;
+
         [Tooltip("The Rigidbody to which the hover force will be applied.")]
         public Rigidbody HoveringRigidbody;
-        [Tooltip("The current height at which the associated Rigidbody can be kept aloft.  Note that the hover force will automatically scale down for lower hover heights.")]
+        [Tooltip("The current height at which the " + nameof(HoverForce.HoveringRigidbody) + " can be kept aloft.  Note that the hover force will automatically scale down for lower hover heights.")]
         public float HoverHeight;
-        [Tooltip("The maximum height at which this " + nameof(UnityEngine.HoverForce) + " will attempt to keep the associated Rigidbody aloft.  Must not be greater than " + nameof(MaxHoverHeight) + ".  Note that the hover force will automatically scale down for lower hover heights.")]
-        public float MaxHoverHeight;
-        [Tooltip("If the ground beneath the hovering Rigidbody makes an angle to the upward direction that is steeper than this angle, then the hover force will not be applied.  This prevents the hovering Rigidbody from 'climbing' steep walls.")]
+        [Tooltip("If the ground beneath the " + nameof(HoverForce.HoveringRigidbody) + " makes an angle to the upward direction that is steeper than this angle, then the hover force will not be applied.  This prevents the " + nameof(HoverForce.HoveringRigidbody) + " from 'climbing' steep walls.")]
         [Range(0f, 90f)]
         public float MaxAngleToSurface;
-        [Tooltip("The maximum mass of associated Rigidbody that this " + nameof(UnityEngine.HoverForce) + " can keep aloft at the " + nameof(MaxHoverHeight) + ".  If set to Infinity, then Rigidbodies of any mass can be kept aloft at the same " + nameof(HoverHeight) + "; otherwise, Rigidbodies more massive than this value will sink to the ground.  Note that the hover force will automatically scale down for lower hover heights and Rigidbody masses.")]
+        [Tooltip("If true, then the hover force will only be applied while the " + nameof(HoverForce.HoveringRigidbody) + " is grounded (i.e., <= " + nameof(HoverForce.HoverHeight) + " units above a surface), causing it to fall at the normal rate.  If false, then the hover force will be applied even when the " + nameof(HoverForce.HoveringRigidbody) + " is not grounded, allowing it to 'float' down more gently.")]
+        public bool OnlyApplyForceWhileGrounded = true;
+        [Tooltip("The maximum mass of " + nameof(HoverForce.HoveringRigidbody) + " that this " + nameof(UnityEngine.HoverForce) + " can keep aloft at the " + nameof(HoverForce.HoverHeight) + ".  If set to Infinity, then Rigidbodies of any mass can be kept aloft at the same " + nameof(HoverHeight) + "; otherwise, Rigidbodies more massive than this value will sink to the ground.  Note that the hover force will automatically scale down for lower " + nameof(HoverForce.HoveringRigidbody) + " masses.")]
         public float MaxHoverableMass;
-        [Tooltip("Only colliders matching this layer mask will be repeled against by the hover force.  That is, the associated Rigidbody will 'fall through' colliders that are not in this layer mask.")]
+        [Tooltip("Only Colliders matching this layer mask will be repelled against by the hover force.  That is, the " + nameof(HoverForce.HoveringRigidbody) + " will 'fall through' colliders that are not in this layer mask.")]
         public LayerMask GroundLayerMask;
-        [Tooltip("What axis should be considered upward?  That is, along what axis will the hover force push the associated Rigidbody to keep it aloft?")]
+        [Tooltip("What axis should be considered upward?  That is, along what axis will the hover force push the " + nameof(HoverForce.HoveringRigidbody) + " to keep it aloft?")]
         public AxisDirection UpwardDirectionType;
         [Tooltip("Only required if " + nameof(UpwardDirectionType) + " is " + nameof(AxisDirection.CustomWorldSpace) + " or " + nameof(AxisDirection.CustomLocalSpace) + ".")]
         public Vector3 CustomUpwardDirection;
+        [Tooltip("If true, then the hover force will be applied using the AddForceAtPosition() method, which looks more realistic when this " + nameof(UnityEngine.HoverForce) + " and the " + nameof(HoverForce.HoveringRigidbody) + " are part of the same in-game entity.  If false, then the hover force will be applied using the AddForce() method, which ignores the position of this " + nameof(UnityEngine.HoverForce) + " relative to the " + nameof(HoverForce.HoveringRigidbody) + ".")]
+        public bool AddForceAtPosition = true;
 
         /// <summary>
         /// Returns the unit vector in which this <see cref="HoverForce"/> will attempt to hover.
@@ -36,17 +40,14 @@ namespace UnityEngine {
                 default: throw new NotImplementedException(BetterLogger.GetSwitchDefault(UpwardDirectionType));
             }
         }
+        public float AppliedFractionOfMaxForce { get; private set; }
 
         private void Reset() {
             HoverHeight = 2f;
-            MaxHoverHeight = 5f;
             MaxAngleToSurface = 60f;
             MaxHoverableMass = 1f;
             UpwardDirectionType = AxisDirection.OppositeGravity;
             CustomUpwardDirection = Vector3.up;
-        }
-        private void Awake() {
-            Assert.IsTrue(HoverHeight <= MaxHoverHeight, $"{this.GetHierarchyNameWithType()} cannot have a {nameof(this.HoverHeight)} higher than its {nameof(this.MaxHoverHeight)}!");
         }
         private void FixedUpdate() {
             if (HoveringRigidbody == null)
@@ -59,22 +60,24 @@ namespace UnityEngine {
             // then apply a hover force to the Rigidbody that scales inversely with the distance from the surface
             Vector3 down = -up;
             Vector3 pos = HoveringRigidbody.position;
-            bool surfaceBelow = Physics.Raycast(pos, down, out RaycastHit hitInfo, HoverHeight, GroundLayerMask, QueryTriggerInteraction.Ignore);
+            bool surfaceBelow = Physics.Raycast(pos, down, out _lastGroundHit, HoverHeight, GroundLayerMask, QueryTriggerInteraction.Ignore);
             if (surfaceBelow) {
-                float angle = Vector3.Angle(up, hitInfo.normal);
+                float angle = Vector3.Angle(up, _lastGroundHit.normal);
                 if (angle <= MaxAngleToSurface)
-                    applyHoverForce(hitInfo, up);
+                    applyHoverForce(up);
             }
         }
 
-        private void applyHoverForce(RaycastHit hitInfo, Vector3 up) {
+        private void applyHoverForce(Vector3 up) {
             float massToLift = Mathf.Min(HoveringRigidbody.mass, MaxHoverableMass);
             float weightToLift = massToLift * Physics.gravity.magnitude;
-            float heightToReach = Mathf.Min(HoverHeight, MaxHoverHeight);
-            float offsetFactor = Mathf.Max(1f - hitInfo.distance / heightToReach, 0f);
-            Vector3 pushForce = weightToLift * (1 + offsetFactor ) * up;
+            AppliedFractionOfMaxForce = Mathf.Max(1f - _lastGroundHit.distance / HoverHeight, 0f);
+            Vector3 pushForce = weightToLift * (1 + AppliedFractionOfMaxForce) * up;
 
-            HoveringRigidbody.AddForce(pushForce);
+            if (AddForceAtPosition)
+                HoveringRigidbody.AddForceAtPosition(pushForce, transform.position);
+            else
+                HoveringRigidbody.AddForce(pushForce);
         }
 
     }
