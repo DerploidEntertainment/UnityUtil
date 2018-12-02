698720a52fa5977e9f26639e53f16e87034141a8
Dan Vicarel <shundra8820@gmail.com>
2018-12-02 13:26:25 -0500

    Added a RandomNumberGenerator component... this has been a long time comin!

---
+++ UnityUtil/RandomNumberGenerator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using S = System;
+
+namespace UnityEngine {
+
+    public sealed class RandomNumberGenerator : MonoBehaviour {
+
+        [Tooltip("Type any string to seed the random number generator, or leave this field blank to use a time-dependent default seed value.")]
+        public string Seed;
+
+        private void Awake() {
+            int seed = string.IsNullOrEmpty(Seed) ? DateTime.Now.GetHashCode() : Seed.GetHashCode();
+            Random.InitState(seed);
+            SystemRand = new S.Random(seed);
+        }
+
+        public S.Random SystemRand { get; private set; }
+
+    }
+
+}
