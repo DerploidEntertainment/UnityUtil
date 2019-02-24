5534c5fac491df9cab79bbbe273aff22e0a2174d
Daniel Vicarel <shundra8820@gmail.com>
2019-02-24 01:31:51 -0500

    Updater is now implemented using a new MultiCollection class

---
+++ UnityUtil/MultiCollection.cs
@@ -0,0 +1,80 @@
+﻿using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace UnityEngine {
+
+    /// <summary>
+    /// Represents a strongly typed list of objects with constant-time insertion, removal, and duplicate checks,
+    /// as well as low-overhead iteration, at the cost of additional memory.
+    /// </summary>
+    /// <typeparam name="TValue">The type of the items in the collection.</typeparam>
+    public class MultiCollection<T> : MultiCollection<T, T>, ICollection<T>, IReadOnlyCollection<T>, IEnumerable<T>, IEnumerable {
+
+        public bool IsReadOnly => false;
+
+        public bool Contains(T item) => _dict.ContainsKey(item);
+        public void Add(T item) => Add(item, item);
+
+    }
+
+    /// <summary>
+    /// Represents a strongly typed list of objects with constant-time insertion, removal, and duplicate checks,
+    /// as well as low-overhead iteration, at the cost of additional memory.
+    /// </summary>
+    /// <typeparam name="TKey">The type of the keys in the collection.</typeparam>
+    /// <typeparam name="TValue">The type of the values in the collection.</typeparam>
+    public class MultiCollection<TKey, TValue> : IEnumerable<TValue> {
+
+        protected class Element {
+            public TKey Key;
+            public TValue Value;
+            public Element(TKey key, TValue value) {
+                Key = key;
+                Value = value;
+            }
+        }
+
+        protected readonly IDictionary<TKey, int> _dict = new Dictionary<TKey, int>();
+        protected readonly List<Element> _list = new List<Element>();
+
+        public int Count => _list.Count;
+
+        public TValue this[TKey key] => _list[_dict[key]].Value;
+        public TValue this[int index] => _list[index].Value;
+
+        public void TrimExcess() => _list.TrimExcess();
+        public void Clear() {
+            _dict.Clear();
+            _list.Clear();
+        }
+        public bool ContainsKey(TKey key) => _dict.ContainsKey(key);
+        public void CopyTo(TValue[] array, int arrayIndex) {
+            for (int i = 0; i < _list.Count; ++i)
+                array[arrayIndex + i] = _list[i].Value;
+        }
+        public bool Remove(TKey key) {
+            bool contained = _dict.TryGetValue(key, out int index);
+            if (!contained)
+                return false;
+
+            Element last = _list[_list.Count - 1];
+            _dict[last.Key] = index;
+            _list[index] = last;
+            _dict.Remove(key);
+            _list.RemoveAt(_list.Count - 1);
+
+            return true;
+        }
+        public void Add(TKey key, TValue value) {
+            _dict.Add(key, _list.Count);
+            _list.Add(new Element(key, value));
+        }
+        public bool TryGetValue(TKey key, out TValue value) => throw new System.NotImplementedException();
+
+        public IEnumerator<TValue> GetEnumerator() => _list.Select(i => i.Value).GetEnumerator();
+        IEnumerator IEnumerable.GetEnumerator() => _list.Select(i => i.Value).GetEnumerator();
+
+    }
+
+}
--- UnityUtil/Updating/Updater.cs
+++ UnityUtil/Updating/Updater.cs
@@ -1,25 +1,15 @@
 ﻿using System;
-using System.Collections.Generic;
 
 namespace UnityEngine {
 
     [DisallowMultipleComponent]
     public class Updater : MonoBehaviour, IUpdater {
 
-        private struct UpdateAction {
-            public int InstanceID;
-            public Action Action;
-        }
-
         private float _tSinceTrim = 0f;
 
-        private IDictionary<int, int> _updateIndices = new Dictionary<int, int>();
-        private IDictionary<int, int> _fixedIndices = new Dictionary<int, int>();
-        private IDictionary<int, int> _lateIndices = new Dictionary<int, int>();
-
-        private List<UpdateAction> _updates = new List<UpdateAction>();
-        private List<UpdateAction> _fixed = new List<UpdateAction>();
-        private List<UpdateAction> _late = new List<UpdateAction>();
+        private readonly MultiCollection<int, Action> _updates = new MultiCollection<int, Action>();
+        private readonly MultiCollection<int, Action> _fixed = new MultiCollection<int, Action>();
+        private readonly MultiCollection<int, Action> _late = new MultiCollection<int, Action>();
 
         // INSPECTOR FIELDS
         [Tooltip("Every time this many seconds passes (in real time, not game time), the update action lists will have their capacities trimmed, if possible, using the List<T>.TrimExcess() method.")]
@@ -30,81 +20,45 @@ namespace UnityEngine {
         public void RegisterUpdate(int instanceID, Action action) {
             if (action == null)
                 throw new ArgumentNullException(nameof(instanceID));
-            if (_updateIndices.ContainsKey(instanceID))
+            if (_updates.ContainsKey(instanceID))
                 throw new ArgumentException($"{this.GetHierarchyNameWithType()} has already registered an object with {nameof(instanceID)} {instanceID} for updates!", nameof(instanceID));
 
-            // Store the new Update action at that index
-            // Store the index of this new Update action
-            _updateIndices.Add(instanceID, _updates.Count);
-            _updates.Add(new UpdateAction() {
-                InstanceID = instanceID,
-                Action = action
-            });
+            _updates.Add(instanceID, action);
         }
         /// <inheritdoc/>
         public void UnregisterUpdate(int instanceID) {
-            if (!_updateIndices.TryGetValue(instanceID, out int index))
-                throw new ArgumentException($"{this.GetHierarchyNameWithType()} cannot unregister the update Action for the object with {nameof(instanceID)} {instanceID} because no such Action was ever registered!", nameof(instanceID));
-
-            UpdateAction lastAction = _updates[_updates.Count - 1];
-            _updateIndices[lastAction.InstanceID] = index;
-            _updates[index] = lastAction;
-            _updates.RemoveAt(_updates.Count - 1);
-            _updateIndices.Remove(instanceID);
+            if (!_updates.Remove(instanceID))
+                throw new ArgumentException($"{this.GetHierarchyNameWithType()} could not unregister the update Action for the object with {nameof(instanceID)} {instanceID} because no such Action was ever registered!", nameof(instanceID));
         }
 
         /// <inheritdoc/>
         public void RegisterFixedUpdate(int instanceID, Action action) {
             if (action == null)
                 throw new ArgumentNullException(nameof(instanceID));
-            if (_fixedIndices.ContainsKey(instanceID))
+            if (_fixed.ContainsKey(instanceID))
                 throw new ArgumentException($"{this.GetHierarchyNameWithType()} has already registered an object with {nameof(instanceID)} {instanceID} for FixedUpdate!", nameof(instanceID));
 
-            // Store the new Update action at that index
-            // Store the index of this new Update action
-            _fixedIndices.Add(instanceID, _fixed.Count);
-            _fixed.Add(new UpdateAction() {
-                InstanceID = instanceID,
-                Action = action
-            });
+            _fixed.Add(instanceID, action);
         }
         /// <inheritdoc/>
         public void UnregisterFixedUpdate(int instanceID) {
-            if (!_fixedIndices.TryGetValue(instanceID, out int index))
-                throw new ArgumentException($"{this.GetHierarchyNameWithType()} cannot unregister the FixedUpdate action for the object with {nameof(instanceID)} {instanceID} because no such action was ever registered!", nameof(instanceID));
-
-            UpdateAction lastAction = _fixed[_fixed.Count - 1];
-            _fixedIndices[lastAction.InstanceID] = index;
-            _fixed[index] = lastAction;
-            _fixed.RemoveAt(_fixed.Count - 1);
-            _fixedIndices.Remove(instanceID);
+            if (!_fixed.Remove(instanceID))
+                throw new ArgumentException($"{this.GetHierarchyNameWithType()} could not unregister the FixedUpdate action for the object with {nameof(instanceID)} {instanceID} because no such action was ever registered!", nameof(instanceID));
         }
 
         /// <inheritdoc/>
         public void RegisterLateUpdate(int instanceID, Action action) {
             if (action == null)
                 throw new ArgumentNullException(nameof(instanceID));
-            if (_lateIndices.ContainsKey(instanceID))
+            if (_late.ContainsKey(instanceID))
                 throw new ArgumentException($"{this.GetHierarchyNameWithType()} has already registered an object with {nameof(instanceID)} {instanceID} for LateUpdate!", nameof(instanceID));
 
-            // Store the new Update action at that index
-            // Store the index of this new Update action
-            _lateIndices.Add(instanceID, _late.Count);
-            _late.Add(new UpdateAction() {
-                InstanceID = instanceID,
-                Action = action
-            });
+            _late.Add(instanceID, action);
         }
         /// <inheritdoc/>
         public void UnregisterLateUpdate(int instanceID) {
-            if (!_lateIndices.TryGetValue(instanceID, out int index))
-                throw new ArgumentException($"{this.GetHierarchyNameWithType()} cannot unregister the LateUpdate action for the object with {nameof(instanceID)} {instanceID} because no such action was ever registered!", nameof(instanceID));
-
-            UpdateAction lastAction = _late[_late.Count - 1];
-            _lateIndices[lastAction.InstanceID] = index;
-            _late[index] = lastAction;
-            _late.RemoveAt(_late.Count - 1);
-            _lateIndices.Remove(instanceID);
+            if (!_late.Remove(instanceID))
+                throw new ArgumentException($"{this.GetHierarchyNameWithType()} could not unregister the LateUpdate action for the object with {nameof(instanceID)} {instanceID} because no such action was ever registered!", nameof(instanceID));
         }
 
         // EVENT HANDLERS
@@ -118,15 +72,15 @@ namespace UnityEngine {
             }
 
             for (int u = 0; u < _updates.Count; ++u)
-                _updates[u].Action?.Invoke();
+                _updates[u]();
         }
         private void FixedUpdate() {
             for (int fu = 0; fu < _fixed.Count; ++fu)
-                _fixed[fu].Action?.Invoke();
+                _fixed[fu]();
         }
         private void LateUpdate() {
             for (int lu = 0; lu < _late.Count; ++lu)
-                _late[lu].Action?.Invoke();
+                _late[lu]();
         }
 
     }
