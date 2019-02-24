2a83c0f720d5300fdb85f82f2c6f2d50a3c1e842
Daniel Vicarel <shundra8820@gmail.com>
2019-02-24 01:37:24 -0500

    WIP: Added a Trigger that can be triggered via a button in the Inspector

---
+++ UnityUtil.Editor/InspectorButtonTriggerDrawer.cs
@@ -0,0 +1,20 @@
+﻿using Sirenix.OdinInspector.Editor;
+using UnityEditor;
+using UnityEngine;
+
+namespace UnityUtil.Editor {
+
+    [CustomEditor(typeof(InspectorButtonTrigger))]
+    public class InspectorButtonTriggerDrawer : OdinEditor {
+
+        public override void OnInspectorGUI() {
+            DrawDefaultInspector();
+
+            var trigger = (InspectorButtonTrigger)target;
+            if (GUILayout.Button(new GUIContent(nameof(trigger.Trigger))))
+                trigger.Trigger();
+        }
+
+    }
+
+}
---
+++ UnityUtil/Triggers/InspectorButtonTrigger.cs
@@ -0,0 +1,14 @@
+﻿using Sirenix.OdinInspector;
+using System;
+
+namespace UnityEngine {
+
+    [Serializable]
+    public class SimpleInspectorEvent : InspectorEvent<EventArgs> { }
+
+    public class InspectorButtonTrigger : MonoBehaviour {
+        public SimpleInspectorEvent Triggered;
+        public void Trigger() => Triggered.Invoke(EventArgs.Empty);
+    }
+
+}
