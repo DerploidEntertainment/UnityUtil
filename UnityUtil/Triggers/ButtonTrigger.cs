f82e8da8114403be001891c330ea20f457e71be2
Dan Vicarel <dvicarel@rakuten.overdrive.com>
2019-03-22 11:59:07 -0400

    The ButtonTrigger now inherits Updatable

--- UnityUtil/Triggers/ButtonTrigger.cs
+++ UnityUtil/Triggers/ButtonTrigger.cs
@@ -1,34 +1,56 @@
-using UnityEngine;
+﻿using UnityEngine;
 using Sirenix.OdinInspector;
 using UnityEngine.Events;
 
 namespace UnityEngine.Triggers {
 
-    public class ButtonTrigger : MonoBehaviour {
+    public class ButtonTrigger : Updatable {
+
+        private float _tRefactory = -1f;
+
         // INSPECTOR FIELDS
         public UnityEvent Triggered = new UnityEvent();
         public bool CanPress = true;
-        public float RefractoryPeriod = 1f;  // seconds
+        [Tooltip("Time, in seconds, before the button may be pressed again.")]
+        public float RefactoryPeriod = 1f;
+
+        protected override void BetterAwake() => RegisterUpdatesAutomatically = false;
+        protected override void BetterOnEnable() {
+            if (_tRefactory > -1f)
+                Updater.RegisterUpdate(InstanceID, updateRefactory);
+        }
+        protected override void BetterOnDisable() {
+            if (_tRefactory > -1f)
+                Updater.UnregisterUpdate(InstanceID);
+        }
 
         // API INTERFACE
         [Button]
-        public void Use() {
-            // Press the button if its refractory period has ended
-            if (CanPress)
-                press();
+        public void Press() {
+            // Don't press the button if its still in the refractory period
+            if (!CanPress)
+                return;
+
+            // Otherwise, raise the trigger event and prevent the button from being pressed for the desired period
+            Triggered.Invoke();
+            CanPress = false;
+            _tRefactory = 0f;
+            Updater.RegisterUpdate(InstanceID, updateRefactory);
         }
 
         // HIDDEN FUNCTIONS
-        private void press() {
-            // Raise the trigger event
-            this.Log($" pressed.");
-            Triggered.Invoke();
+        private void updateRefactory() {
+            if (_tRefactory <= RefactoryPeriod) {
+                _tRefactory += Time.deltaTime;
+                return;
+            }
 
-            // Prevent the button from being pressed for the desired period
-            CanPress = false;
-            Invoke(nameof(reset), RefractoryPeriod);
+            _tRefactory = 0f;
+            Updater.UnregisterUpdate(InstanceID);
+
+            CanPress = true;
         }
-        private void reset() => CanPress = true;
+
     }
 
 }
