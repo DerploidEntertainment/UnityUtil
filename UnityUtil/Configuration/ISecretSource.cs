4231babc37de50c405e63faa9faca8f36b45b8e3
Dan Vicarel <dvicarel@rakuten.overdrive.com>
2019-04-05 16:37:41 -0400

    Added an abstract SecretsLoader class
    Specific games can override SaveSecretsAsync on this class in order to save secrets during automated builds

---
+++ UnityUtil/Configuration/ISecretSource.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace UnityEngine {
+
+    public interface ISecretSource {
+
+        Task<string> LoadSecrets(CancellationToken cancellationToken);
+
+    }
+
+}
---
+++ UnityUtil/Configuration/SecretsLoader.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace UnityEngine {
+
+    public abstract class SecretsLoader {
+
+        public void LoadSecrets(IEnumerable<ISecretSource> secretSources) {
+            var async = new AsyncCaller();
+            async.CallAsync(async ct => {
+                string[] secrets = await Task.WhenAll(secretSources.Select(s => s.LoadSecrets(ct)));
+                secrets = secrets.Where(s => !string.IsNullOrWhiteSpace(s)).ToArray();
+                if (!ct.IsCancellationRequested)
+                    await SaveSecretsAsync(secrets, ct);
+            });
+        }
+
+        protected abstract Task SaveSecretsAsync(string[] secrets, CancellationToken cancellationToken);
+
+    }
+
+}
