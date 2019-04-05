410ecf2e8966e89445124af95a4b9be61ae81547
Dan Vicarel <dvicarel@rakuten.overdrive.com>
2019-04-05 12:12:37 -0400

    Cleaned up the InjectDrawer a bit
    I finally figured out why this drawer isn't having any effect though... it's because the [Inject]ed fields are all non-public and therefore not being drawn in the Inspector! Unfortunately, there's no good way around this with an attribute, as showing in the Inspector would require serializing the field, and we don't that... May have to add some custom drawer for MonoBehaviours that draws all [Inject]ed fields...idk

--- UnityUtil.Editor/InjectDrawer.cs
+++ UnityUtil.Editor/InjectDrawer.cs
@@ -10,29 +10,23 @@ namespace UnityUtil.Editor {
 
         // Draw the property inside the given rect
         public override void OnGUI(Rect position, SerializedProperty property, GUIContent label) {
-            // Using BeginProperty/EndProperty on this property will allow prefab override logic
-            EditorGUI.BeginProperty(position, label, property);
-
             // Cache GUI properties that will need to be reset at the end
             int origIndent = EditorGUI.indentLevel;
             EditorGUI.indentLevel = 0;
             Color origColor = GUI.contentColor;
             GUI.contentColor = Color.yellow;
+            string tag = (attribute as InjectAttribute).Tag;
 
             // Draw Label
-            position = EditorGUI.PrefixLabel(position, GUIUtility.GetControlID(FocusType.Passive), label);
-
-            // Draw fields
-            string tag = (attribute as InjectAttribute).Tag;
-            string serviceStr = "Dependency" + (string.IsNullOrEmpty(tag) ? string.Empty : $", requires tag '{tag}'");
+            GUI.enabled = false;
             var serviceRect = new Rect(position.x, position.y, FIELD_WIDTH, position.height);
-            var serviceLbl = new GUIContent(serviceStr);
-            position = EditorGUI.PrefixLabel(serviceRect, GUIUtility.GetControlID(FocusType.Passive), serviceLbl);
+            string serviceTxt = $"{label.text}: Dependency {(string.IsNullOrEmpty(tag) ? string.Empty : $", requires tag '{tag}'")}";
+            EditorGUI.SelectableLabel(serviceRect, serviceTxt);
 
             // Clean up
+            GUI.enabled = true;
             GUI.contentColor = origColor;
             EditorGUI.indentLevel = origIndent;
-            EditorGUI.EndProperty();
         }
     }
 
