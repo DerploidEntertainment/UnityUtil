af8216b1e5e58f30a23c630b854b3278885c8489
Daniel Vicarel <shundra8820@gmail.com>
2019-04-13 16:40:17 -0400

    Added an ErrorAlert component:
    This service represents an error alert within a game's UI, as opposed to error messages that only get logged

---
+++ UnityUtil/ErrorAlert.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using UnityEngine.Assertions;
+using UnityEngine.UI;
+
+namespace UnityEngine {
+
+    public class ErrorAlert : MonoBehaviour {
+
+        public Text Text;
+
+        private void Awake() {
+            Assert.IsNotNull(Text, this.GetDependencyAssertion(nameof(Text)));
+        }
+
+        public void ShowError(string message) {
+            Text.text = message;
+        }
+        public void ShowException(Exception ex) {
+            Text.text = ex.Message;
+        }
+
+    }
+
+}
