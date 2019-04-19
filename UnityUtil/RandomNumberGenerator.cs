e8dd06de8e0c5551e3b43c1dc423bd9f031f1e6d
Dan Vicarel <dvicarel@rakuten.overdrive.com>
2019-04-18 22:03:38 -0400

    RandomNumberGenerator is now a Configurable

--- UnityUtil/RandomNumberGenerator.cs
+++ UnityUtil/RandomNumberGenerator.cs
@@ -3,13 +3,23 @@ using S = System;
 
 namespace UnityEngine {
 
-    public sealed class RandomNumberGenerator : MonoBehaviour {
+    public sealed class RandomNumberGenerator : Configurable {
 
         [Tooltip("Type any string to seed the random number generator, or leave this field blank to use a time-dependent default seed value.")]
         public string Seed;
 
-        private void Awake() {
-            int seed = string.IsNullOrEmpty(Seed) ? DateTime.Now.GetHashCode() : Seed.GetHashCode();
+        protected override void OnAwake() {
+            int seed;
+            if (string.IsNullOrEmpty(Seed)) {
+                seed = DateTime.Now.GetHashCode();
+                Seed = seed.ToString();
+            }
+            else {
+                bool isInt = int.TryParse(Seed, out seed);
+                if (!isInt)
+                    seed = Seed.GetHashCode();
+            }
+
             Random.InitState(seed);
             SystemRand = new S.Random(seed);
         }
