dafffc1a2e1436fa6aae87eab3b2865d7c41b5ad
Dan Vicarel <shundra8820@gmail.com>
2019-12-17 02:24:06 -0500

    Added EditMode tests for all the new logging code!
    - Didn't add tests for TypeNameLogEnricher or FrameCountLogEnricher, since those are so straightforward
    - Also didn't add tests for ObjectNameLogEnricher that just duplicate tests for the GetHierarchyName methods
    - EditModeTestHelpers now includes a few more methods to help with log assertions (cause that whole system is so freakin confusing...). This also required adding a reference to UnityEngine.TestRunner in UnityUtil.Editor, so we might need to do some more MSBuild cleanup shortly...

---
+++ UnityUtil/Assets/UnityUtil/Editor/Test.EditMode/Logging/ObjectNameLogEnricherTest.cs
@@ -0,0 +1,103 @@
+﻿using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.Logging;
+using UnityUtil.Editor;
+
+namespace UnityUtil.Test.EditMode.Logging {
+
+    public class ObjectNameLogEnricherTest {
+
+        [Test]
+        public void NonUnityObjectsReturnEmpty() {
+            EditModeTestHelpers.ResetScene();
+
+            ObjectNameLogEnricher enricher = getObjectNameLogEnricher();
+            string log;
+
+            object objSource = new object();
+            log = enricher.GetEnrichedLog(objSource);
+            Assert.That(log, Is.Empty);
+
+            string strSource = "a string";
+            log = enricher.GetEnrichedLog(strSource);
+            Assert.That(log, Is.Empty);
+
+            int intSource = 0;
+            log = enricher.GetEnrichedLog(intSource);
+            Assert.That(log, Is.Empty);
+
+            var classSource = new ObjectNameLogEnricherTest();
+            log = enricher.GetEnrichedLog(classSource);
+            Assert.That(log, Is.Empty);
+        }
+
+        [Test]
+        public void ScriptableObjectsReturnName() {
+            EditModeTestHelpers.ResetScene();
+
+            ObjectNameLogEnricher enricher = getObjectNameLogEnricher();
+            string log;
+
+            AudioClip audioClip = AudioClip.Create("audio", lengthSamples: 1, channels: 1, frequency: 1000, stream: false);
+            log = enricher.GetEnrichedLog(audioClip);
+            Assert.That(log, Is.EqualTo("audio"));
+
+            var avatarMask = new AvatarMask { name = "avatarMask" };
+            log = enricher.GetEnrichedLog(avatarMask);
+            Assert.That(log, Is.EqualTo("avatarMask"));
+
+            ObjectNameLogEnricher enricherSource = ScriptableObject.CreateInstance<ObjectNameLogEnricher>();
+            enricherSource.name = "enricher-asset";
+            log = enricher.GetEnrichedLog(enricherSource);
+            Assert.That(log, Is.EqualTo("enricher-asset"));
+        }
+
+        [Test]
+        public void GameObjectsReturnName() {
+            EditModeTestHelpers.ResetScene();
+
+            ObjectNameLogEnricher enricher = getObjectNameLogEnricher();
+            GameObject obj;
+            string log;
+
+            obj = new GameObject("obj0");
+            log = enricher.GetEnrichedLog(obj);
+            Assert.That(log, Is.EqualTo("obj0"));
+
+            obj = new GameObject("obj1");
+            log = enricher.GetEnrichedLog(obj);
+            Assert.That(log, Is.EqualTo("obj1"));
+        }
+
+        [Test]
+        public void ComponentsOnSameGameObjectReturnSame() {
+            EditModeTestHelpers.ResetScene();
+
+            ObjectNameLogEnricher enricher = getObjectNameLogEnricher(numParents: 1, ancestorNameSeparator: ">");
+            var parent = new GameObject("parent");
+            var obj = new GameObject("obj");
+            obj.transform.parent = parent.transform;
+            Component component;
+            string log;
+
+            component = obj.AddComponent<AudioSource>();
+            log = enricher.GetEnrichedLog(component);
+            Assert.That(log, Is.EqualTo("parent>obj"));
+
+            component = obj.AddComponent<Animator>();
+            log = enricher.GetEnrichedLog(component);
+            Assert.That(log, Is.EqualTo("parent>obj"));
+        }
+
+        private ObjectNameLogEnricher getObjectNameLogEnricher(uint numParents = 1u, string ancestorNameSeparator = ">", string formatString = "{0}") {
+            ObjectNameLogEnricher enricher = ScriptableObject.CreateInstance<ObjectNameLogEnricher>();
+            enricher.NumParents = numParents;
+            enricher.AncestorNameSeparator = ancestorNameSeparator;
+            enricher.FormatString = formatString;
+
+            return enricher;
+        }
+
+    }
+
+}
---
+++ UnityUtil/Assets/UnityUtil/Editor/Test.EditMode/Logging/UnityObjectExtensionsTest.cs
@@ -0,0 +1,197 @@
+﻿using NUnit.Framework;
+using System.Text.RegularExpressions;
+using UnityEngine;
+using UnityEngine.Logging;
+using UnityEngine.TestTools;
+using UnityUtil.Editor;
+
+namespace UnityUtil.Test.EditMode.Logging {
+
+    public class UnityObjectExtensionsTest {
+
+        [Test]
+        public void CanGetHierarchyName_DiffNumParents() {
+            EditModeTestHelpers.ResetScene();
+
+            GameObject obj = getGameObject("test", numParents: 5);
+            string name;
+
+            name = UnityObjectExtensions.GetHierarchyName(obj, numParents: 0);
+            Assert.That(name, Is.EqualTo("'test'"));
+
+            name = UnityObjectExtensions.GetHierarchyName(obj, numParents: 1);
+            Assert.That(name, Is.EqualTo("'parent4>test'"));
+
+            name = UnityObjectExtensions.GetHierarchyName(obj, numParents: 2);
+            Assert.That(name, Is.EqualTo("'parent3>parent4>test'"));
+        }
+
+        [Test]
+        public void CanGetHierarchyName_DiffSeparators() {
+            EditModeTestHelpers.ResetScene();
+
+            GameObject obj = getGameObject("test", numParents: 1);
+            string name;
+
+            name = UnityObjectExtensions.GetHierarchyName(obj, numParents: 1, separator: ">");
+            Assert.That(name, Is.EqualTo("'parent0>test'"));
+
+            name = UnityObjectExtensions.GetHierarchyName(obj, numParents: 1, separator: ".");
+            Assert.That(name, Is.EqualTo("'parent0.test'"));
+
+            name = UnityObjectExtensions.GetHierarchyName(obj, numParents: 1, separator: " | ");
+            Assert.That(name, Is.EqualTo("'parent0 | test'"));
+        }
+
+        [Test]
+        public void CanGetHierarchyName_DiffFormatStrings() {
+            EditModeTestHelpers.ResetScene();
+
+            GameObject obj = getGameObject("test", numParents: 1);
+            string name;
+
+            name = UnityObjectExtensions.GetHierarchyName(obj, numParents: 1, formatString: "");
+            Assert.That(name, Is.EqualTo(""));
+
+            name = UnityObjectExtensions.GetHierarchyName(obj, numParents: 1, formatString: "{0}");
+            Assert.That(name, Is.EqualTo("parent0>test"));
+
+            name = UnityObjectExtensions.GetHierarchyName(obj, numParents: 1, formatString: "'{0}'");
+            Assert.That(name, Is.EqualTo("'parent0>test'"));
+
+            name = UnityObjectExtensions.GetHierarchyName(obj, numParents: 1, formatString: "<{0}>");
+            Assert.That(name, Is.EqualTo("<parent0>test>"));
+        }
+
+        [Test]
+        public void CanGetHierarchyName_IncludesOnlyNumParentsThatExist() {
+            EditModeTestHelpers.ResetScene();
+
+            GameObject obj = getGameObject("test", numParents: 2);
+            string name;
+
+            name = UnityObjectExtensions.GetHierarchyName(obj, numParents: 2);
+            Assert.That(name, Is.EqualTo("'parent0>parent1>test'"));
+
+            name = UnityObjectExtensions.GetHierarchyName(obj, numParents: 3);
+            Assert.That(name, Is.EqualTo("'parent0>parent1>test'"));
+
+            name = UnityObjectExtensions.GetHierarchyName(obj, numParents: 10);
+            Assert.That(name, Is.EqualTo("'parent0>parent1>test'"));
+        }
+
+        [Test]
+        public void CanGetHierarchyName_SameForAllAttachedComponents() {
+            EditModeTestHelpers.ResetScene();
+
+            GameObject obj = getGameObject("test", numParents: 1);
+            Component component;
+            string name;
+
+            component = obj.transform;
+            name = UnityObjectExtensions.GetHierarchyName(component, numParents: 1);
+            Assert.That(name, Is.EqualTo("'parent0>test'"));
+
+            component = obj.AddComponent<AudioSource>();
+            name = UnityObjectExtensions.GetHierarchyName(component, numParents: 1);
+            Assert.That(name, Is.EqualTo("'parent0>test'"));
+
+            component = obj.AddComponent<Animator>();
+            name = UnityObjectExtensions.GetHierarchyName(component, numParents: 1);
+            Assert.That(name, Is.EqualTo("'parent0>test'"));
+        }
+
+        [Test]
+        public void CanGetHierarchyNameWithType_DiffComponents() {
+            EditModeTestHelpers.ResetScene();
+
+            GameObject obj = getGameObject("test", numParents: 1);
+            Component component;
+            string name;
+
+            component = obj.transform;
+            name = UnityObjectExtensions.GetHierarchyNameWithType(component, numParents: 1);
+            Assert.That(name, Is.EqualTo("Transform 'parent0>test'"));
+
+            component = obj.AddComponent<AudioSource>();
+            name = UnityObjectExtensions.GetHierarchyNameWithType(component, numParents: 1);
+            Assert.That(name, Is.EqualTo("AudioSource 'parent0>test'"));
+
+            component = obj.AddComponent<Animator>();
+            name = UnityObjectExtensions.GetHierarchyNameWithType(component, numParents: 1);
+            Assert.That(name, Is.EqualTo("Animator 'parent0>test'"));
+        }
+
+        [Test]
+        public void CanGetHierarchyNameWithType_DiffFormatString() {
+            EditModeTestHelpers.ResetScene();
+
+            GameObject obj = getGameObject("test", numParents: 1);
+            string name;
+
+            name = UnityObjectExtensions.GetHierarchyNameWithType(obj.transform, numParents: 1, formatString: "");
+            Assert.That(name, Is.EqualTo(""));
+
+            name = UnityObjectExtensions.GetHierarchyNameWithType(obj.transform, numParents: 1, formatString: "{0}");
+            Assert.That(name, Is.EqualTo("Transform"));
+
+            name = UnityObjectExtensions.GetHierarchyNameWithType(obj.transform, numParents: 1, formatString: "{1}");
+            Assert.That(name, Is.EqualTo("parent0>test"));
+
+            name = UnityObjectExtensions.GetHierarchyNameWithType(obj.transform, numParents: 1, formatString: "{0}{1}");
+            Assert.That(name, Is.EqualTo("Transformparent0>test"));
+
+            name = UnityObjectExtensions.GetHierarchyNameWithType(obj.transform, numParents: 1, formatString: "component '{1}' (type {0})");
+            Assert.That(name, Is.EqualTo("component 'parent0>test' (type Transform)"));
+        }
+
+        [Test]
+        public void CanAssertActiveAndEnabled() {
+            EditModeTestHelpers.ResetScene();
+
+            GameObject obj = getGameObject("test", numParents: 0);
+            Behaviour behaviour = obj.AddComponent<Animator>();
+
+            obj.name = EditModeTestHelpers.GetUniqueLog("none");
+            obj.SetActive(true);
+            behaviour.enabled = true;
+            behaviour.AssertAcitveAndEnabled();
+            LogAssert.NoUnexpectedReceived();
+
+            obj.name = EditModeTestHelpers.GetUniqueLog("Behaviour");
+            obj.SetActive(true);
+            behaviour.enabled = false;
+            behaviour.AssertAcitveAndEnabled();
+            EditModeTestHelpers.ExpectLog(LogType.Assert, new Regex($".*{Regex.Escape(obj.name)}.*disabled"));
+
+            obj.name = EditModeTestHelpers.GetUniqueLog("GameObject");
+            obj.SetActive(false);
+            behaviour.enabled = true;
+            behaviour.AssertAcitveAndEnabled();
+            EditModeTestHelpers.ExpectLog(LogType.Assert, new Regex($".*{Regex.Escape(obj.name)}.*inactive"));
+
+            obj.name = EditModeTestHelpers.GetUniqueLog("GameObject-Behaviour");
+            obj.SetActive(false);
+            behaviour.enabled = false;
+            behaviour.AssertAcitveAndEnabled();
+            LogAssert.Expect(LogType.Assert, new Regex($".*{Regex.Escape(obj.name)}.*inactive"));
+            LogAssert.Expect(LogType.Assert, new Regex($".*{Regex.Escape(obj.name)}.*disabled"));
+            LogAssert.NoUnexpectedReceived();
+        }
+
+        private GameObject getGameObject(string name, int numParents = 1, string parentNameFormatString = "parent{0}") {
+            Transform lastParentTrans = null;
+            for (int p = 0; p < numParents; ++p) {
+                Transform t = new GameObject(string.Format(parentNameFormatString, p)).transform;
+                t.parent = lastParentTrans;
+                lastParentTrans = t;
+            }
+
+            var obj = new GameObject(name);
+            obj.transform.parent = lastParentTrans;
+            return obj;
+        }
+
+    }
+
+}
