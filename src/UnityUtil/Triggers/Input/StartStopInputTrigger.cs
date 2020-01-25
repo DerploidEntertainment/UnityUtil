dfda7a5f67a8e28b0e606c035d00277f0f44d7be
Dan Vicarel <shundra8820@gmail.com>
2020-01-25 02:28:41 -0500

    Added a StartStopInputTrigger

---
+++ src/UnityUtil/Triggers/Input/StartStopInputTrigger.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine.Events;
+using UnityEngine.Inputs;
+
+namespace UnityEngine.Triggers.Input {
+
+    public class StartStopInputTrigger : Updatable {
+
+        public StartStopInput Input;
+        public UnityEvent InputStarted = new UnityEvent();
+        public UnityEvent InputStopped = new UnityEvent();
+
+        protected override void BetterAwake() {
+            RegisterUpdatesAutomatically = true;
+            BetterUpdate = checkInputs;
+        }
+
+        private void checkInputs(float deltaTime) {
+            if (Input.Started())
+                InputStarted.Invoke();
+            else if (Input.Stopped())
+                InputStopped.Invoke();
+        }
+
+    }
+
+}
