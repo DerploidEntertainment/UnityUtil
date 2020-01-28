99e311d2904778d1c075691c841d0d66d0ab835b
Dan Vicarel <shundra8820@gmail.com>
2020-01-27 23:32:17 -0500

    Added an OpenableUri asset type

---
+++ src/UnityUtil/OpenableUri.cs
@@ -0,0 +1,9 @@
+﻿namespace UnityEngine {
+    [CreateAssetMenu(fileName = "uri", menuName = "UnityUtil" + "/" + nameof(UnityEngine.OpenableUri))]
+    public class OpenableUri : ScriptableObject {
+        [Tooltip("The URI to be opened, using any of the protocols supported by Unit's Application.OpenURL API.")]
+        [TextArea]
+        public string Uri = "http://www.example.com";
+        public void Open() => Application.OpenURL(Uri);
+    }
+}
