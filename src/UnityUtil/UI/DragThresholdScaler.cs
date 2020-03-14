3db4cb87ba06d1617725a0ceb1397510d7d328b9
Dan Vicarel <shundra8820@gmail.com>
2020-03-14 00:50:12 -0400

    Added a DragThresholdScaler component
    This allows EventSystem.pixelDragThreshold values to scale correctly with different screen pixel densities... at least I think, haven't really tested it on any other devices...

---
+++ src/UnityUtil/UI/DragThresholdScaler.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine.EventSystems;
+using UnityEngine.Logging;
+
+namespace UnityEngine.UI {
+    /// <summary>
+    /// This class is used to automatically scale an <see cref="EventSystem"/>'s <see cref="EventSystem.pixelDragThreshold"/> with respect to a <see cref="Canvas"/>.
+    /// </summary>
+    /// <remarks>
+    /// See the comments by user @runevision on this Unity forum post for more info: https://forum.unity.com/threads/buttons-within-scroll-rect-are-difficult-to-press-on-mobile.265682/
+    /// </remarks>
+    public class DragThresholdScaler : Configurable {
+
+        private const string TOOLTIP = nameof(EventSystem) + "'s " + nameof(EventSystems.EventSystem.pixelDragThreshold) + " will be scaled by the product of " + nameof(DragThresholdFactor) + " and " + nameof(CanvasScaler) + "'s " + nameof(UI.CanvasScaler.scaleFactor) + ". This means that the " + nameof(EventSystems.EventSystem.pixelDragThreshold) + " will scale as needed on screens with different pixel densities.";
+
+        [Tooltip(TOOLTIP)]
+        public EventSystem EventSystem;
+        [Tooltip(TOOLTIP)]
+        public CanvasScaler CanvasScaler;
+        [Min(0f), Tooltip(TOOLTIP)]
+        public int DragThresholdFactor;
+
+        private void Reset() => DragThresholdFactor = 5;
+
+        protected override void OnAwake() {
+            base.OnAwake();
+
+            this.AssertAssociation(EventSystem, nameof(EventSystem));
+            this.AssertAssociation(CanvasScaler, nameof(CanvasScaler));
+
+            EventSystem.pixelDragThreshold = DragThresholdFactor * (int)CanvasScaler.scaleFactor;
+        }
+
+    }
+}
