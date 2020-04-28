5acf24532c9c85c6a2fada0adcceaba04f213d37
Dan Vicarel <shundra8820@gmail.com>
2020-04-28 16:17:40 -0400

    Focus/Pause/Quit events handled by new ApplicationLifecycleTrigger component
    Removed similar pause logic from the GameStateManager

--- src/UnityUtil/GameStateManager.cs
+++ src/UnityUtil/GameStateManager.cs
@@ -1,54 +1,25 @@
-﻿using UnityEngine.Events;
-using UnityEngine.Inputs;
-using UnityEngine.Logging;
-using UnityEngine.SceneManagement;
+﻿using UnityEngine.SceneManagement;
 
-namespace UnityEngine {
+namespace UnityEngine
+{
 
     [DisallowMultipleComponent]
-    public class GameStateManager : MonoBehaviour {
-
-        private ILogger _logger;
+    public class GameStateManager : Configurable {
 
         // INSPECTOR INTERFACE
-        [Tooltip("The input to use to toggle the paused state of the game.  If not set, then the game can only be paused programmatically.")]
-        public StartStopInput TogglePauseInput;
         public LoadSceneMode LoadSceneMode = LoadSceneMode.Single;
-        public UnityEvent Paused = new UnityEvent();
-        public UnityEvent Unpaused = new UnityEvent();
-
-        private void Inject(ILoggerProvider loggerProvider) => _logger = loggerProvider.GetLogger(this);
-        private void Awake() => DependencyInjector.ResolveDependenciesOf(this);
 
         // API INTERFACE
-        public bool IsPaused { get; private set; }
-        public void SetPaused(bool paused) {
-            // Adjust the paused state
-            IsPaused = paused;
-            Time.timeScale = IsPaused ? 0f : 1f;
-
-            // Raise the corresponding event
-            _logger.Log($" {(IsPaused ? "paused" : "resumed")} the game.", context: this);
-            (IsPaused ? Paused : Unpaused).Invoke();
-        }
+        #pragma warning disable CA1822 // Mark members as static
         public void LoadScene(string sceneName) => SceneManager.LoadScene(sceneName, LoadSceneMode);
         public void LoadSceneAsync(string sceneName) => SceneManager.LoadSceneAsync(sceneName, LoadSceneMode);
         public void UnloadSceneAsync(string sceneName) => SceneManager.UnloadSceneAsync(sceneName);
-        public void RestartScene() {
-            // Unpause and reload the active Scene
-            SetPaused(false);
-            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
-        }
-        public void Quit() => Application.Quit();
+        public void RestartScene() => SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+
         public void SetCursorConfined() => Cursor.lockState = CursorLockMode.Confined;
         public void SetCursorLocked() => Cursor.lockState = CursorLockMode.Locked;
         public void SetCursorUnlocked() => Cursor.lockState = CursorLockMode.None;
-
-        // EVENT HANDLERS
-        private void Update() {
-            if (TogglePauseInput?.Started() ?? false)
-                SetPaused(!IsPaused);
-        }
+        #pragma warning restore CA1822 // Mark members as static
 
     }
 
---
+++ src/UnityUtil/Triggers/ApplicationLifecycleTriggers.cs
@@ -0,0 +1,40 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using UnityEngine.Events;
+using UnityEngine.Logging;
+
+namespace UnityEngine.Triggers
+{
+    public class ApplicationLifecycleTriggers : Configurable
+    {
+        private ILogger _logger;
+
+        public UnityEvent Focused = new UnityEvent();
+        public UnityEvent Unfocused = new UnityEvent();
+        public UnityEvent Paused = new UnityEvent();
+        public UnityEvent Unpaused = new UnityEvent();
+        public UnityEvent Quitting = new UnityEvent();
+
+        public void Inject(ILoggerProvider loggerProvider) => _logger = loggerProvider.GetLogger(this);
+
+        [SuppressMessage("Code Quality", "IDE0051:Remove unused private members", Justification = "Unity message")]
+        private void OnApplicationFocus(bool hasFocus)
+        {
+            _logger.Log($"Application {(hasFocus ? "focused" : "blurred")}", context: this);
+            (hasFocus ? Focused : Unfocused).Invoke();
+        }
+
+        [SuppressMessage("Code Quality", "IDE0051:Remove unused private members", Justification = "Unity message")]
+        private void OnApplicationPause(bool pauseStatus)
+        {
+            _logger.Log($"Application {(pauseStatus ? "" : "un")}paused", context: this);
+            (pauseStatus ? Paused : Unpaused).Invoke();
+        }
+
+        [SuppressMessage("Code Quality", "IDE0051:Remove unused private members", Justification = "Unity message")]
+        private void OnApplicationQuit()
+        {
+            _logger.Log($"Application quitting...", context: this);
+            Quitting.Invoke();
+        }
+    }
+}
