95e4a3e19bd8ad90b60234fb9724daf01e150f7e
Dan Vicarel <shundra8820@gmail.com>
2020-08-01 11:22:22 -0400

    CopyrightText now uses a multiline input

--- src/UnityUtil/Legal/CopyrightText.cs
+++ src/UnityUtil/Legal/CopyrightText.cs
@@ -7,6 +7,7 @@ using UnityEngine.UI;
 namespace UnityEngine.Legal {
     public class CopyrightText : Configurable {
         [Tooltip("This string is used to populate " + nameof(Text) + ". '{0}' will be replaced with the current date and '{1}' will be replaced with " + nameof(Application.companyName) + ", using .NET composite formatting. For example, '{0:yyyy}' would be replaced with just the current 4-digit year. See here for details: https://docs.microsoft.com/en-us/dotnet/standard/base-types/composite-formatting")]
+        [Multiline]
         public string FormatString;
         public Text Text;
 
