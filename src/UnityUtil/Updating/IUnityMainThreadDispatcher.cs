488e6190c124386eea8d7226c5d25c2984f983c1
Dan Vicarel <shundra8820@gmail.com>
2020-09-07 22:20:29 -0400

    Added a UnityMainThreadDispatcher service
    Current intended use is for enqueuing work on Unity's main thread from native events

---
+++ src/UnityUtil/Updating/IUnityMainThreadDispatcher.cs
@@ -0,0 +1,46 @@
+﻿/*
+ * This file copied from Pim de Witte's original repo on GitHub: https://github.com/PimDeWitte/UnityMainThreadDispatcher
+ * That repo is licensed under the Apache License 2.0: https://spdx.org/licenses/Apache-2.0.html
+ * That license requires me to document changes, so... 
+ *      - I made a couple tweaks to satisfy Visual Studio's compiler/style warnings
+ *      - It now derives from the UnityUtil Updatable class so that its part of our custom, managed update loop
+ *      - Added it to the UnityEngine namespace, like the rest of UnityUtil
+ *      - Split out its public methods into a separate interface for dependency injection
+*/
+
+using System;
+using System.Collections;
+using System.Threading.Tasks;
+
+namespace UnityEngine {
+
+    /// <summary>
+    /// Original author: Pim de Witte (pimdewitte.com) and contributors, https://github.com/PimDeWitte/UnityMainThreadDispatcher.
+    /// Encapsulates a thread-safe queue with actions to execute on the next update loop.
+    /// It can be used to make calls to Unity's main thread for things such as UI manipulation.
+    /// It was developed for use in combination with the Firebase Unity plugin, which uses separate threads for event handling.
+    /// </summary>
+    public interface IUnityMainThreadDispatcher {
+
+        /// <summary>
+        /// Locks the queue and adds the IEnumerator to the queue
+        /// </summary>
+        /// <param name="action">IEnumerator function that will be executed from Unity's main thread.</param>
+        void Enqueue(IEnumerator action);
+
+        /// <summary>
+        /// Locks the queue and adds <paramref name="action"/> to the queue
+        /// </summary>
+        /// <param name="action">Action that will be executed from Unity's main thread.</param>
+        void Enqueue(Action action);
+
+        /// <summary>
+        /// Locks the queue and adds <paramref name="action"/> to the queue, returning a <see cref="Task"/> which is completed when the action completes
+        /// </summary>
+        /// <param name="action">Action that will be executed from Unity's main thread.</param>
+        /// <returns>A <see cref="Task"/> that can be awaited until the action completes</returns>
+        public Task EnqueueAsync(Action action);
+
+    }
+
+}
---
+++ src/UnityUtil/Updating/UnityMainThreadDispatcher.cs
@@ -0,0 +1,83 @@
+﻿/*
+ * This file copied from Pim de Witte's original repo on GitHub: https://github.com/PimDeWitte/UnityMainThreadDispatcher
+ * That repo is licensed under the Apache License 2.0: https://spdx.org/licenses/Apache-2.0.html
+ * That license requires me to document changes, so... 
+ *      - I made a couple tweaks to satisfy Visual Studio's compiler/style warnings
+ *      - It now derives from the UnityUtil Updatable class so that its part of our custom, managed update loop
+ *      - Added it to the UnityEngine namespace, like the rest of UnityUtil
+ *      - Split out its public methods into a separate interface for dependency injection
+*/
+
+using System.Collections;
+using System.Collections.Generic;
+using System;
+using System.Threading.Tasks;
+using System.Diagnostics.CodeAnalysis;
+
+namespace UnityEngine {
+
+    /// <summary>
+    /// Original author: Pim de Witte (pimdewitte.com) and contributors, https://github.com/PimDeWitte/UnityMainThreadDispatcher.
+    /// A thread-safe class which holds a queue with actions to execute on the next update loop.
+    /// It can be used to make calls to Unity's main thread for things such as UI manipulation.
+    /// It was developed for use in combination with the Firebase Unity plugin, which uses separate threads for event handling.
+    /// </summary>
+    public class UnityMainThreadDispatcher : Updatable, IUnityMainThreadDispatcher {
+
+        private static readonly Queue<Action> s_actionQueue = new Queue<Action>();
+
+        private void processActionQueue(float deltaTime) {
+            lock (s_actionQueue) {
+                while (s_actionQueue.Count > 0) {
+                    s_actionQueue.Dequeue().Invoke();
+                }
+            }
+        }
+
+        /// <inheritdoc/>
+        public void Enqueue(IEnumerator action) {
+            lock (s_actionQueue) {
+                s_actionQueue.Enqueue(() => {
+                    StartCoroutine(action);
+                });
+            }
+        }
+
+        /// <inheritdoc/>
+        public void Enqueue(Action action) => Enqueue(actionWrapper(action));
+
+        /// <inheritdoc/>
+        public Task EnqueueAsync(Action action) {
+            var tcs = new TaskCompletionSource<bool>();
+
+            Enqueue(actionWrapper(() => {
+                try {
+                    action();
+                    tcs.TrySetResult(true);
+                }
+                catch (Exception ex) {
+                    tcs.TrySetException(ex);
+                }
+            }));
+
+            return tcs.Task;
+        }
+
+
+        private IEnumerator actionWrapper(Action action) {
+            action();
+            yield return null;
+        }
+
+
+        [SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "Unity message")]
+        protected override void Awake() {
+            base.Awake();
+
+            BetterUpdate = processActionQueue;
+            RegisterUpdatesAutomatically = true;
+        }
+
+    }
+
+}
