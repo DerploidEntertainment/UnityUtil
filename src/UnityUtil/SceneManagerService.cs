2690a6ec673cb6b91edae1388acfe921c49094e1
Dan Vicarel <shundra8820@gmail.com>
2021-01-12 01:20:52 -0500

    Split up GameStateManager into SceneManagerService and CursorManager
    Neither new class inherits from Configurable cause why ever did it

---
+++ src/UnityUtil/SceneManagerService.cs
@@ -0,0 +1,22 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using UnityEngine.SceneManagement;
+
+namespace UnityEngine
+{
+
+    public class SceneManagerService : MonoBehaviour {
+
+        public LoadSceneMode LoadSceneMode = LoadSceneMode.Single;
+
+        public void LoadScene(string sceneName) => SceneManager.LoadScene(sceneName, LoadSceneMode);
+
+        public void LoadSceneAsync(string sceneName) => SceneManager.LoadSceneAsync(sceneName, LoadSceneMode);
+
+        [SuppressMessage("Performance", "CA1822:Mark members as static", Justification = "UnityEvents can't call static methods")]
+        public void UnloadSceneAsync(string sceneName) => SceneManager.UnloadSceneAsync(sceneName);
+
+        [SuppressMessage("Performance", "CA1822:Mark members as static", Justification = "UnityEvents can't call static methods")]
+        public void RestartScene() => SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+}
