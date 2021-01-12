8adf7c0fd15d651253300405854e5a7e36aec15f
Dan Vicarel <shundra8820@gmail.com>
2021-01-12 01:23:45 -0500

    Added a ConfigurationSourceCollection asset for prioritizing ConfigurationSources
    Useful for build scripts, which before were just loading ConfigurationSources in the order detected by Resources.Load

---
+++ src/UnityUtil/Configuration/ConfigurationSourceCollection.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+
+namespace UnityEngine
+{
+    [CreateAssetMenu(menuName = nameof(UnityUtil) + "/" + "Configuration" + "/" + nameof(ConfigurationSourceCollection), fileName = "configuration-sources.asset")]
+    public class ConfigurationSourceCollection : ScriptableObject
+    {
+        [Tooltip(
+            "Sources must be provided in reverse order of importance (i.e., configs in source 0 will override configs in source 1, " +
+            "which will override configs in source 2, etc.)"
+        )]
+        public ConfigurationSource[] ConfigurationSources;
+
+        [SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "Unity message")]
+        private void Reset() => ConfigurationSources = Array.Empty<ConfigurationSource>();
+    }
+}
