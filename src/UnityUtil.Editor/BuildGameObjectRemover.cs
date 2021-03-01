0e7fe17be9aee170dc4b05520fb381ce0464c34b
Dan Vicarel <shundra8820@gmail.com>
2021-03-01 01:29:35 -0500

    Added a system to conditionally remove GameObjects from builds!

---
+++ src/UnityUtil.Editor/BuildGameObjectRemover.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.Logging;
+using UnityEngine.SceneManagement;
+
+namespace UnityUtil.Editor
+{
+    public class BuildGameObjectRemover
+    {
+        private readonly ILogger _logger;
+
+        public BuildGameObjectRemover(ILoggerProvider loggerProvider) => _logger = loggerProvider.GetLogger(this);
+
+        public void RemoveGameObjectsFromScene(Scene scene, List<RemoveFromBuild> removeTargetBuffer = null)
+        {
+            RuntimePlatform platform = Application.platform;
+            BuildContext buildContext =
+                Application.isPlaying ? BuildContext.PlayMode :
+                Debug.isDebugBuild ? BuildContext.DebugBuilds :
+                BuildContext.ReleaseBuilds;
+
+            int numRemoveTargets = 0;
+            removeTargetBuffer ??= new List<RemoveFromBuild>(100);  // This is just a wild-a$$ guess. If consumers want a reasonably sized buffer, they should pass in their own
+            GameObject[] roots = scene.GetRootGameObjects();
+            for (int r = 0; r < roots.Length; ++r) {
+                GameObject root = roots[r];
+
+                root.GetComponentsInChildren(includeInactive: true, removeTargetBuffer);
+                int removeTargetsUnderRoot = removeTargetBuffer.Count;
+                numRemoveTargets += removeTargetsUnderRoot;
+
+                // Determine which GameObjects should actually be removed
+                RemoveFromBuild[] targetsToRemove = removeTargetBuffer
+                    .Where(x => !x.PreservePlatforms.Contains(platform) || (x.PreserveBuildContexts & buildContext) == 0)
+                    .ToArray();
+
+                // Remove them (and/or their children)!
+                for (int t = 0; t < targetsToRemove.Length; ++t) {
+                    RemoveFromBuild removeTarget = targetsToRemove[t];
+                    Transform removeTrans = removeTarget.transform;
+                    if (removeTarget.DestroyBehavior == DestroyBehavior.ChildrenOnly) {
+                        for (int ch = 0; ch < removeTrans.childCount; ++ch)
+                            Object.DestroyImmediate(removeTrans.GetChild(ch));
+                    }
+                    else if (removeTarget.DestroyBehavior == DestroyBehavior.SelfAndChildren)
+                        Object.DestroyImmediate(removeTrans.gameObject);
+                }
+
+                if (removeTargetsUnderRoot > 0)
+                    _logger.Log($"Removed {targetsToRemove.Length} GameObjects under root object '{root.name}' in scene '{scene.name}', out of {removeTargetsUnderRoot} marked for contextual removal.");
+            }
+
+
+            if (numRemoveTargets == 0)
+                _logger.Log($"No GameObjects marked for contextual removal in scene '{scene.name}'.");
+        }
+    }
+}
---
+++ src/UnityUtil/RemoveFromBuild.cs
@@ -0,0 +1,49 @@
+﻿using Sirenix.OdinInspector;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace UnityEngine
+{
+    [Flags]
+    public enum BuildContext
+    {
+        PlayMode = 0b001,
+        DebugBuilds = 0b010,
+        ReleaseBuilds = 0b100,
+        AllBuilds = 0b111,
+    }
+
+    public enum DestroyBehavior
+    {
+        SelfAndChildren,
+        ChildrenOnly,
+    }
+    public class RemoveFromBuild : MonoBehaviour
+    {
+        public const DestroyBehavior DefaultDestroyBehavior = DestroyBehavior.SelfAndChildren;
+
+        private const string MSG_PRESERVE_CONTEXT =
+            "This GameObject (and/or its children) will be removed from the build unless the build context matches " +
+            nameof(PreserveBuildContexts) + " AND the platform matches " + nameof(PreservePlatforms) + ".";
+
+        [InfoBox("This component will not actually affect your scene files; it will only remove this GameObject and/or its children from BUILDS.")]
+        [InfoBox(
+            "This component will NOT affect prefabs instantiated at runtime. " +
+            "Any GameObjects with " + nameof(RemoveFromBuild) + " components in prefabs will NOT be removed at runtime unless YOU do so explicitly.",
+            InfoMessageType = InfoMessageType.Warning
+        )]
+
+        [Tooltip("What should be destroyed during builds?")]
+        public DestroyBehavior DestroyBehavior = DefaultDestroyBehavior;
+
+        [Tooltip(MSG_PRESERVE_CONTEXT)]
+        public BuildContext PreserveBuildContexts;
+
+        [Tooltip(MSG_PRESERVE_CONTEXT + " Order does not matter.")]
+        public List<RuntimePlatform> PreservePlatforms;
+
+        [Conditional("UNITY_EDITOR")]
+        public void Reset() => DestroyBehavior = DefaultDestroyBehavior;
+    }
+}
