12e52f63b872272e1b6caef8d7e459131bdcceaa
Dan Vicarel <shundra8820@gmail.com>
2021-11-12 22:18:08 -0500

    Added a PushDetonator.SafeRigidbody property
    Can't remember why I was working on this...

--- src/UnityUtil/Physics/PushDetonator.cs
+++ src/UnityUtil/Physics/PushDetonator.cs
@@ -1,7 +1,8 @@
-﻿using System.Linq;
-using UnityEngine;
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Linq;
 
-namespace UnityEngine {
+namespace UnityEngine
+{
 
     [RequireComponent(typeof(Detonator))]
     public class PushDetonator : MonoBehaviour {
@@ -12,7 +13,12 @@ namespace UnityEngine {
         public float ExplosionForce = 10f;
         public float ExplosionUpwardsModifier = 2f;
 
+        [Tooltip("This rigidbody is 'safe' from pushing. Useful if this detonator has a relationship with a rigidbody such that the rigidbody should not be pushed.")]
+        public Rigidbody SafeRigidbody;
+
         // EVENT HANDLERS
+        [SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "Unity message")]
+        [SuppressMessage("CodeQuality", "IDE0051:Remove unused private members", Justification = "Unity message")]
         private void Awake() {
             _detonator = GetComponent<Detonator>();
             _detonator.Detonated.AddListener(pushAll);
@@ -22,10 +28,11 @@ namespace UnityEngine {
         private void pushAll(Collider[] colliders) {
             // Apply an explosion force to all unique Rigidbodies among these Colliders
             // Upwards modifier adjusts to gravity
-            Rigidbody[] rigidbodies = colliders.Select(c => c.attachedRigidbody)
-                                        .Where(rb => rb != null)
-                                        .Distinct()
-                                        .ToArray();
+            Rigidbody[] rigidbodies = colliders
+                .Select(c => c.attachedRigidbody)
+                .Where(rb => rb != null && rb != SafeRigidbody)
+                .Distinct()
+                .ToArray();
             for (int rb = 0; rb < rigidbodies.Length; ++rb) {
                 Rigidbody rigidbody = rigidbodies[rb];
                 Vector3 explosionPos = _detonator.transform.position + ExplosionUpwardsModifier * Physics.gravity.normalized;
