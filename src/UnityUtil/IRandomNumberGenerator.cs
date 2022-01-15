104db75e7a90b9a181d04584ed326e4ece88e0da
Dan Vicarel <shundra8820@gmail.com>
2022-01-15 17:15:06 -0500

    Added IRandomNumberGenerator.Next and .Range methods
    These have default interface implementations, so no existing implementations are broken. Really these methods are just hooks for mocking in unit tests...long overdue!

--- src/UnityUtil/IRandomNumberGenerator.cs
+++ src/UnityUtil/IRandomNumberGenerator.cs
@@ -1,11 +1,19 @@
-﻿using S = System;
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using S = System;
 
-namespace UnityEngine {
+namespace UnityEngine
+{
 
-    public interface IRandomNumberGenerator {
-
+    [SuppressMessage("Security", "CA5394:Do not use insecure randomness", Justification = "Interface is not intended to be secure")]
+    public interface IRandomNumberGenerator
+    {
         string Seed { get; }
         S.Random? SystemRand { get; }
+
+        int NextInt() => SystemRand?.Next() ?? throw new InvalidOperationException($"{nameof(SystemRand)} must be non-null before calling {nameof(NextInt)}.");
+        int Range(int inclusiveMin, int exclusiveMax) =>
+            SystemRand?.Next(inclusiveMin, exclusiveMax) ?? throw new InvalidOperationException($"{nameof(SystemRand)} must be non-null before calling {nameof(NextInt)}.");
 
     }
 
