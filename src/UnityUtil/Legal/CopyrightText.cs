185529f20cbe9780a962dbd685a0758200c94cf0
Dan Vicarel <shundra8820@gmail.com>
2022-04-27 21:57:31 -0400

    Tweaked CopyrightText.FormatString tooltip

--- src/UnityUtil/Legal/CopyrightText.cs
+++ src/UnityUtil/Legal/CopyrightText.cs
@@ -9,7 +9,8 @@ public class CopyrightText : Configurable
 {
     [Tooltip(
         $"This string is used to populate {nameof(Text)}. " +
-        $"'{{0}}' will be replaced with the current date (in user's culture) and '{{1}}' will be replaced with {nameof(Device.Application.companyName)}, " +
+        $"'{{0}}' will be replaced with the current date (in user's culture) and " +
+        $"'{{1}}' will be replaced with {nameof(Device.Application)}.{nameof(Device.Application.companyName)}, " +
         $"using .NET composite formatting. For example, '{{0:yyyy}}' would be replaced with just the current 4-digit year. " +
         $"See here for details: https://docs.microsoft.com/en-us/dotnet/standard/base-types/composite-formatting"
     )]
