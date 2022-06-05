9fb5a3a5d4dede8852cc2650865b89cfd3c4aed2
Dan Vicarel <shundra8820@gmail.com>
2022-06-05 14:31:45 -0400

    PlayerPrefsLocalCache now includes app identifer in pref keys
    Allows multiple Derploid Unity projects and built players to run on same machine

--- src/UnityUtil/Storage/PlayerPrefsLocalCache.cs
+++ src/UnityUtil/Storage/PlayerPrefsLocalCache.cs
@@ -3,16 +3,18 @@
 public class PlayerPrefsLocalCache : ILocalCache
 {
     public void DeleteAll() => PlayerPrefs.DeleteAll();
-    public void DeleteKey(string key) => PlayerPrefs.DeleteKey(key);
-    public float GetFloat(string key, float defaultValue) => PlayerPrefs.GetFloat(key, defaultValue);
-    public float GetFloat(string key) => PlayerPrefs.GetFloat(key);
-    public int GetInt(string key, int defaultValue) => PlayerPrefs.GetInt(key, defaultValue);
-    public int GetInt(string key) => PlayerPrefs.GetInt(key);
-    public string GetString(string key, string defaultValue) => PlayerPrefs.GetString(key, defaultValue);
-    public string GetString(string key) => PlayerPrefs.GetString(key);
-    public bool HasKey(string key) => PlayerPrefs.HasKey(key);
+    public void DeleteKey(string key) => PlayerPrefs.DeleteKey(getFullKey(key));
+    public float GetFloat(string key, float defaultValue) => PlayerPrefs.GetFloat(getFullKey(key), defaultValue);
+    public float GetFloat(string key) => PlayerPrefs.GetFloat(getFullKey(key));
+    public int GetInt(string key, int defaultValue) => PlayerPrefs.GetInt(getFullKey(key), defaultValue);
+    public int GetInt(string key) => PlayerPrefs.GetInt(getFullKey(key));
+    public string GetString(string key, string defaultValue) => PlayerPrefs.GetString(getFullKey(key), defaultValue);
+    public string GetString(string key) => PlayerPrefs.GetString(getFullKey(key));
+    public bool HasKey(string key) => PlayerPrefs.HasKey(getFullKey(key));
     public void Save() => PlayerPrefs.Save();
-    public void SetFloat(string key, float value) => PlayerPrefs.SetFloat(key, value);
-    public void SetInt(string key, int value) => PlayerPrefs.SetInt(key, value);
-    public void SetString(string key, string value) => PlayerPrefs.SetString(key, value);
+    public void SetFloat(string key, float value) => PlayerPrefs.SetFloat(getFullKey(key), value);
+    public void SetInt(string key, int value) => PlayerPrefs.SetInt(getFullKey(key), value);
+    public void SetString(string key, string value) => PlayerPrefs.SetString(getFullKey(key), value);
+
+    private static string getFullKey(string key) => $"{Application.identifier}_{key}";
 }
