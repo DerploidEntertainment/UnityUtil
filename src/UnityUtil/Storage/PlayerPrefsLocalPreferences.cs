d73a4598961a55b6ed3b92634a054551e1c2cc4f
Dan Vicarel <shundra8820@gmail.com>
2022-06-24 02:26:23 -0400

    PlayerPrefsLocalPreferences now uses app version in keys
    Rather than app identifier, since keys can change between versions, and the app identifier is already part of PlayerPrefs keys

--- src/UnityUtil/Storage/PlayerPrefsLocalPreferences.cs
+++ src/UnityUtil/Storage/PlayerPrefsLocalPreferences.cs
@@ -2,6 +2,13 @@
 
 public class PlayerPrefsLocalPreferences : ILocalPreferences
 {
+    private readonly IAppVersion _appVersion;
+
+    public PlayerPrefsLocalPreferences(IAppVersion appVersion)
+    {
+        _appVersion = appVersion;
+    }
+
     public void DeleteAll() => PlayerPrefs.DeleteAll();
     public void DeleteKey(string key) => PlayerPrefs.DeleteKey(getFullKey(key));
     public float GetFloat(string key, float defaultValue) => PlayerPrefs.GetFloat(getFullKey(key), defaultValue);
@@ -16,5 +23,5 @@ public class PlayerPrefsLocalPreferences : ILocalPreferences
     public void SetInt(string key, int value) => PlayerPrefs.SetInt(getFullKey(key), value);
     public void SetString(string key, string value) => PlayerPrefs.SetString(getFullKey(key), value);
 
-    private static string getFullKey(string key) => $"{Application.identifier}_{key}";
+    private string getFullKey(string key) => $"{_appVersion.Version}_{key}";
 }
