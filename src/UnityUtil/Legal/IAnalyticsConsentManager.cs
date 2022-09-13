c374178777d1efeca7b96a237336db5448603784
Dan Vicarel <dvic@derploid.com>
2022-09-12 23:48:37 -0400

    Add IAnalyticsConsentManager interface

---
+++ src/UnityUtil/Legal/IAnalyticsConsentManager.cs
@@ -0,0 +1,11 @@
+﻿using System.Threading.Tasks;
+
+namespace UnityEngine.Legal;
+
+public interface IAnalyticsConsentManager
+{
+    bool ForceAnalyticsConsentBehavior { get; set; }
+    DataConsentState CheckAnalyticsConsent();
+    void RequestAnalyticsConsent();
+    Task InitializeAnalyticsAsync();
+}
