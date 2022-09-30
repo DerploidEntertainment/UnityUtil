9ef5c44bacc51b3fe2c9458bd10e8dd0f79a8e5a
Dan Vicarel <dvic@derploid.com>
2022-09-30 17:40:06 -0400

    Adjust the Configurable.ConfigKey tooltip

--- src/UnityUtil/Configuration/Configurable.cs
+++ src/UnityUtil/Configuration/Configurable.cs
@@ -9,15 +9,14 @@ public abstract class Configurable : MonoBehaviour
     protected IConfigurator? Configurator;
     protected ILogger? Logger;
 
-    public const string ConfigKeyTooltip =
+    [Tooltip(
         "The key by which to look up configuration for this Component. " +
         "A blank string (default) is equivalent to the fully qualified type name. " +
         "Leading/trailing whitespace is ignored. For example, all components of type 'Derp' in the namespace 'MyGame', " +
         "will use the default config key 'MyGame.Derp', and their field config keys will have the form 'MyGame.Derp.<fieldname>'. " +
-        "If you want a particular 'Derp' instance to be individually configurable, then you must give it a unique config key, and" +
-        "then its field config keys would have the form '<configkey>.<fieldname>'.";
-
-    [Tooltip(ConfigKeyTooltip)]
+        "If you want a particular 'Derp' instance to be individually configurable, then you must give it a unique config key, and " +
+        "then its field config keys would have the form '<configkey>.<fieldname>'."
+    )]
     public string ConfigKey = "";
 
 #if UNITY_EDITOR
