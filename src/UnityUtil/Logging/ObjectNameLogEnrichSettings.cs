1540471b398fa2544d3f54e0f7666634e46a5227
Dan Vicarel <shundra8820@gmail.com>
2022-12-13 00:51:30 -0500

    Add a BaseUnityUtilLogger from which app- and namespace-specific loggers can derive
    Injects an ObjectNameLogEnrichSettings instance, so it can add a log scope for the logging Unity object's name

--- src/UnityUtil/Logging/ObjectNameLogEnrichSettings.cs
+++ src/UnityUtil/Logging/ObjectNameLogEnrichSettings.cs
@@ -3,7 +3,7 @@
 namespace UnityUtil.Logging;
 
 [CreateAssetMenu(menuName = $"{nameof(UnityUtil)}/{nameof(Logging)}/{nameof(ObjectNameLogEnricher)}", fileName = "object-name-log-enricher")]
-public class ObjectNameLogEnricher : LogEnricher
+public class ObjectNameLogEnrichSettings : ScriptableObject
 {
     [Tooltip(
         "For Objects that are scene objects, the name will include up to this many parent objects' names in the logs. " +
@@ -19,11 +19,4 @@ public class ObjectNameLogEnricher : LogEnricher
         "Read more about .NET composite formatting here: https://docs.microsoft.com/en-us/dotnet/standard/base-types/composite-formatting"
     )]
     public string FormatString = UnityObjectExtensions.DefaultHierarchyNameFormatString;
-
-    public override string GetEnrichedLog(object source) =>
-        source is not Object sourceObj
-            ? string.Empty
-        : source is not Component component
-            ? sourceObj.name
-        : component.GetHierarchyName(NumParents, AncestorNameSeparator, FormatString);
 }
