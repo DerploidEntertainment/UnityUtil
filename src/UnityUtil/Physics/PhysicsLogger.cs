56e3ec8bf3fa20e0d67b722b8f854767789c4f3d
Dan Vicarel <shundra8820@gmail.com>
2022-12-14 03:45:44 -0500

    Migrate Physics and Inventories namespaces to custom logger pattern

---
+++ src/UnityUtil/Physics/PhysicsLogger.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.Logging;
+using UnityEngine;
+using UnityUtil.Logging;
+using static Microsoft.Extensions.Logging.LogLevel;
+
+namespace UnityUtil.Physics;
+
+/// <inheritdoc/>
+internal class PhysicsLogger<T> : BaseUnityUtilLogger<T>
+{
+    public PhysicsLogger(ILoggerFactory loggerFactory, T context)
+        : base(loggerFactory, context, eventIdOffset: 10_000) { }
+
+    #region Information
+
+    #endregion
+
+    #region Warning
+
+    public void DuplicateColliderFail(Collider collider) =>
+        Log(id: 0, nameof(DuplicateColliderFail), Warning, $"{{{nameof(collider)}}} is not a BoxCollider, SphereCollider, or CapsuleCollider, so it will not be duplicated", collider.GetHierarchyName());
+
+    #endregion
+
+    #region Error
+
+    #endregion
+
+}
