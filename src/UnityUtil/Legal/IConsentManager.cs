49bade531aff1e196103344b6cde39bf782dcb70
Dan Vicarel <shundra8820@gmail.com>
2023-08-27 02:20:58 -0400

    Apparently IConsentManager was never converted to a file-scoped namespace?

--- src/UnityUtil/Legal/IConsentManager.cs
+++ src/UnityUtil/Legal/IConsentManager.cs
@@ -1,26 +1,25 @@
 ﻿using System;
 
-namespace UnityUtil.Legal
+namespace UnityUtil.Legal;
+
+public interface IConsentManager
 {
-    public interface IConsentManager
-    {
-        bool ForceConsentBehavior { get; }
+    bool ForceConsentBehavior { get; }
 
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="initializableWithConsent"></param>
-        /// <returns></returns>
-        /// <exception cref="InvalidOperationException">Consent has not yet been initialized</exception>
-        /// <exception cref="ArgumentException"><paramref name="initializableWithConsent"/> is not managed by this consent manager</exception>
-        bool HasConsent(IInitializableWithConsent initializableWithConsent);
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="initializableWithConsent"></param>
+    /// <returns></returns>
+    /// <exception cref="InvalidOperationException">Consent has not yet been initialized</exception>
+    /// <exception cref="ArgumentException"><paramref name="initializableWithConsent"/> is not managed by this consent manager</exception>
+    bool HasConsent(IInitializableWithConsent initializableWithConsent);
 
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="initializableWithConsent"></param>
-        /// <exception cref="InvalidOperationException">Consent has not yet been initialized</exception>
-        /// <exception cref="ArgumentException"><paramref name="initializableWithConsent"/> is not managed by this consent manager</exception>
-        void OptOut(IInitializableWithConsent initializableWithConsent);
-    }
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="initializableWithConsent"></param>
+    /// <exception cref="InvalidOperationException">Consent has not yet been initialized</exception>
+    /// <exception cref="ArgumentException"><paramref name="initializableWithConsent"/> is not managed by this consent manager</exception>
+    void OptOut(IInitializableWithConsent initializableWithConsent);
 }
