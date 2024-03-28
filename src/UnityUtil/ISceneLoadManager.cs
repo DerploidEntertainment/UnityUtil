186ac531070a15f291c09f1a37f28991b247d306
Dan Vicarel <dan.vicarel@gmail.com>
2024-03-28 02:59:56 -0400

    Overhaul SceneManagerService to new SceneLoadManager class
    - New name is less awkward
    - Implements a new interface that other classes can inject to (un)load scenes
    - Every method is tagged with Odin's `[Button]` for invoking from the Inspector
    - Changed the component to a ScriptableObject since its logic is scene-agnostic

---
+++ src/UnityUtil/ISceneLoadManager.cs
@@ -0,0 +1,11 @@
+﻿namespace UnityUtil;
+
+public interface ISceneLoadManager
+{
+    void LoadAdditiveScene(string sceneName);
+    void LoadAdditiveSceneAsync(string sceneName);
+    void LoadSingleScene(string sceneName);
+    void LoadSingleSceneAsync(string sceneName);
+    void RestartScene();
+    void UnloadSceneAsync(string sceneName);
+}
---
+++ src/UnityUtil/SceneLoadManager.cs
@@ -0,0 +1,27 @@
+﻿using Sirenix.OdinInspector;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace UnityUtil;
+
+[CreateAssetMenu(menuName = $"{nameof(UnityUtil)}/{nameof(SceneLoadManager)}", fileName = "scene-load-manager")]
+public class SceneLoadManager : ScriptableObject, ISceneLoadManager
+{
+    [Button]
+    public void LoadSingleScene(string sceneName) => SceneManager.LoadScene(sceneName, LoadSceneMode.Single);
+
+    [Button]
+    public void LoadSingleSceneAsync(string sceneName) => SceneManager.LoadSceneAsync(sceneName, LoadSceneMode.Single);
+
+    [Button]
+    public void LoadAdditiveScene(string sceneName) => SceneManager.LoadScene(sceneName, LoadSceneMode.Additive);
+
+    [Button]
+    public void LoadAdditiveSceneAsync(string sceneName) => SceneManager.LoadSceneAsync(sceneName, LoadSceneMode.Additive);
+
+    [Button]
+    public void UnloadSceneAsync(string sceneName) => SceneManager.UnloadSceneAsync(sceneName);
+
+    [Button]
+    public void RestartScene() => SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+}
