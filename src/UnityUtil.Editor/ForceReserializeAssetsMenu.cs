e64d9aff4af24bcb06662252b5816d725afc82f3
Dan Vicarel <dan.vicarel@gmail.com>
2024-04-04 00:45:12 -0400

    Add a menu item for forcibly reserializing all assets

---
+++ src/UnityUtil.Editor/ForceReserializeAssetsMenu.cs
@@ -0,0 +1,14 @@
+﻿using UnityEditor;
+
+namespace UnityUtil.Editor;
+
+public static class ForceReserializeAssetsMenu
+{
+    /// <summary>
+    /// This is mainly meant to be called after upgrading a project to a new Unity version,
+    /// but see <a href="https://docs.unity3d.com/ScriptReference/AssetDatabase.ForceReserializeAssets.html">the docs</a>
+    /// for other use cases and examples.
+    /// </summary>
+    [MenuItem("UnityUtil/Force-Reserialize All Assets (Expensive!!)")]
+    public static void ForceReserializeAllAssets() => AssetDatabase.ForceReserializeAssets();
+}
