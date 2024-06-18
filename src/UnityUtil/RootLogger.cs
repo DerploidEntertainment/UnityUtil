b5b6df8e28a03bb254ce0b565e5f2a1a4a05ff05
Dan Vicarel <dan.vicarel@gmail.com>
2024-06-18 12:59:32 -0400

    Improve some scene-related logs in RootLogger

--- src/UnityUtil/RootLogger.cs
+++ src/UnityUtil/RootLogger.cs
@@ -19,7 +19,7 @@ internal class RootLogger<T> : BaseUnityUtilLogger<T>
 
     public void RegisteredService(Service service, Scene? scene)
     {
-        string msg = $"Successfully registered service of type {{serviceType}} and tag {{tag}}{(scene.HasValue ? $" from {{{nameof(scene)}}}" : "")}";
+        string msg = $"Successfully registered service of type '{{serviceType}}' and tag '{{tag}}'{(scene.HasValue ? $" from scene '{{{nameof(scene)}}}'" : "")}";
         LogInformation(id: 0, nameof(RegisteredService), msg, service.ServiceType.Name, service.InjectTag, scene?.name);
     }
 
@@ -27,10 +27,10 @@ internal class RootLogger<T> : BaseUnityUtilLogger<T>
         LogInformation(id: 1, nameof(ToggledRecordingDependencyResolution), $"Recording dependency resolutions: {{{nameof(isRecording)}}}", isRecording);
 
     public void UnregisteringSceneServices(Scene scene) =>
-        LogInformation(id: 2, nameof(UnregisteringSceneServices), $"Unregistering services from {{{nameof(scene)}}}...", scene.name);
+        LogInformation(id: 2, nameof(UnregisteringSceneServices), $"Unregistering services from scene '{{{nameof(scene)}}}'...", scene.name);
 
     public void UnregisteredAllSceneServices(Scene scene, int count) =>
-        LogInformation(id: 3, nameof(UnregisteredAllSceneServices), $"Successfully unregistered all {{{nameof(count)}}} services from {{{nameof(scene)}}}", count, scene.name);
+        LogInformation(id: 3, nameof(UnregisteredAllSceneServices), $"Successfully unregistered all {{{nameof(count)}}} services from scene '{{{nameof(scene)}}}'", count, scene.name);
 
     public void ResolvedMethodServiceParameter(string clientName, string tag, ParameterInfo parameter) =>
         LogInformation(id: 4, nameof(ResolvedMethodServiceParameter), $"{{{nameof(clientName)}}} had dependency of Type {{serviceType}}{(string.IsNullOrEmpty(tag) ? "" : $" with tag {{{nameof(tag)}}}")} injected into {{{nameof(parameter)}}}", clientName, parameter.ParameterType.FullName, tag, parameter.Name);
@@ -67,7 +67,7 @@ internal class RootLogger<T> : BaseUnityUtilLogger<T>
     #region Warning
 
     public void UnregisterMissingSceneService(Scene scene) =>
-        LogWarning(id: 0, nameof(UnregisterMissingSceneService), $"Cannot unregister services from {{{nameof(scene)}}}, as none have been registered. Are you trying to destroy multiple service collections from the same scene?", scene.name);
+        LogWarning(id: 0, nameof(UnregisterMissingSceneService), $"Cannot unregister services from scene '{{{nameof(scene)}}}' as none have been registered. Are you trying to destroy multiple service collections from the same scene?", scene.name);
 
     public void MethodHasMultipleDependenciesOfType(string clientName, Type serviceType, string tag) =>
         LogWarning(id: 1, nameof(MethodHasMultipleDependenciesOfType), $"{{{nameof(clientName)}}} has multiple dependencies of Type {{{nameof(serviceType)}}} with tag {{{nameof(tag)}}}", clientName, serviceType.FullName, tag);
