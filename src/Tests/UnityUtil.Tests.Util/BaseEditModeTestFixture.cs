fb535e1b3f65253539521f9bb39753cfac363dfa
Dan Vicarel <dan.vicarel@gmail.com>
2024-06-20 01:06:41 -0400

    Separate edit/play mode tests into package-specific projects
    These tests will be copied to new subfolders of Assets/Tests/ in the Unity project, so they aren't included in the actual UPM packages

---
+++ src/Tests/UnityUtil.Tests.Util/BaseEditModeTestFixture.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+using UnityEngine;
+
+namespace UnityUtil.Editor.Tests
+{
+    public class BaseEditModeTestFixture
+    {
+        [OneTimeSetUp]
+        public void OneTimeSetUp()
+        {
+            EditModeTestHelpers.ResetScene();
+            Debug.Log($"Scene reset by {nameof(BaseEditModeTestFixture)}.{nameof(OneTimeSetUp)}");
+        }
+    }
+}
