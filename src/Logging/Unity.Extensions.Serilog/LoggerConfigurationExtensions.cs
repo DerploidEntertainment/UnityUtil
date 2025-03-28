228e6232d79cc1442732b4804325dede4e0797d6
Dan Vicarel <dan.vicarel@gmail.com>
2025-03-28 15:44:23 -0400

    Reorganize solution folders to make shared .props files easier
    Also move the `$(UnityDestinationFolder)` property and `package.json` Item definitions for UnityUtil packages to the new `UnityUtil/Directory.Build.props` file (with overrides for any projects that don't fit the pattern, like `UnityUtil.Editor`)

---
+++ src/Logging/Unity.Extensions.Serilog/LoggerConfigurationExtensions.cs
@@ -0,0 +1,72 @@
+﻿using Serilog;
+using Serilog.Debugging;
+using Serilog.Enrichers.Unity;
+using Serilog.Events;
+using Serilog.Formatting;
+using Serilog.Sinks.Unity;
+using UnityEngine;
+using UE = UnityEngine;
+
+namespace Unity.Extensions.Logging;
+
+/// <summary>
+/// Extends <see cref="LoggerConfiguration"/> with methods to add Unity enricher, sink, and other configuration.
+/// </summary>
+public static class LoggerConfigurationExtensions
+{
+    /// <summary>
+    /// Add the Unity Console to the Serilog pipeline.
+    /// </summary>
+    /// <param name="loggerConfiguration">Logger configuration.</param>
+    /// <param name="textFormatter"><inheritdoc cref="UnitySink(ITextFormatter, UE.ILogger, UnitySinkSettings)" path="/param[@name='textFormatter']"/></param>
+    /// <param name="logger"><inheritdoc cref="UnitySink(ITextFormatter, UE.ILogger, UnitySinkSettings)" path="/param[@name='logger']"/></param>
+    /// <param name="selfLogIsUnityLogWarning">
+    /// Whether Serilog's <see cref="SelfLog"/> is set to Unity's <see cref="Debug.LogWarning(object)"/>.
+    /// Useful during development, but may generate noisy warnings if trying to destructure <see cref="Object"/>s in <see cref="LogEvent"/>s.
+    /// </param>
+    /// <param name="setMinimumLevelFromUnityFilterLogType">
+    /// Whether to set <see cref="LoggerConfiguration.MinimumLevel"/> based on Unity's <see cref="UnityEngine.ILogger.filterLogType"/>.
+    /// </param>
+    /// <param name="unityLogEnricherSettings"><inheritdoc cref="UnityLogEnricher(UnityLogEnricherSettings)" path="/param[@name='unityLogEnricherSettings']"/></param>
+    /// <param name="unitySinkSettings">
+    /// <inheritdoc cref="UnitySink(ITextFormatter, UE.ILogger, UnitySinkSettings)" path="/param[@name='unitySinkSettings']"/>
+    /// By default, <see cref="UnitySinkSettings.UnityTagLogProperty"/> is set to <c>SourceContext</c> to reuse the type name set by <c>Serilog</c>
+    /// as the Unity log <c>tag</c>.
+    /// </param>
+    /// <returns>Configuration object allowing method chaining.</returns>
+    public static LoggerConfiguration AddUnity(
+        this LoggerConfiguration loggerConfiguration,
+        ITextFormatter textFormatter,
+        UE.ILogger? logger = null,
+        bool selfLogIsUnityLogWarning = true,
+        bool setMinimumLevelFromUnityFilterLogType = true,
+        UnityLogEnricherSettings? unityLogEnricherSettings = null,
+        UnitySinkSettings? unitySinkSettings = null
+    )
+    {
+        unitySinkSettings ??= new UnitySinkSettings {
+            UnityTagLogProperty = "SourceContext",  // This log property automatically added by Serilog, contain logging type's name
+        };
+
+        if (selfLogIsUnityLogWarning)
+            SelfLog.Enable(Debug.LogWarning);
+
+        if (setMinimumLevelFromUnityFilterLogType) {
+            loggerConfiguration = loggerConfiguration.MinimumLevel.Is(
+                Debug.unityLogger.filterLogType switch {
+                    LogType.Log => LogEventLevel.Information,
+                    LogType.Warning => LogEventLevel.Warning,
+                    LogType.Assert or LogType.Error or LogType.Exception => LogEventLevel.Error,
+                    _ => LogEventLevel.Information
+                }
+            );
+        }
+
+        if (unitySinkSettings.UnityContextLogProperty is not null)
+            loggerConfiguration = loggerConfiguration.Destructure.With(new UnityLogContextDestructuringPolicy());
+
+        return loggerConfiguration
+            .Enrich.WithUnityData(unityLogEnricherSettings ?? new UnityLogEnricherSettings())
+            .WriteTo.Unity(textFormatter, logger, unitySinkSettings);
+    }
+}
---
+++ src/Logging/Unity.Extensions.Serilog/UnityLogContextDestructuringPolicy.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using Serilog.Context;
+using Serilog.Core;
+using Serilog.Events;
+using UnityEngine;
+using UE = UnityEngine;
+
+namespace Unity.Extensions.Serilog;
+
+/// <summary>
+/// Policy for destructuring <see cref="ValueTuple{T}"/> instances with a single <see cref="UE.Object"/> item, representing the <c>context</c> for a Unity log.
+/// <para>
+/// Provides a mechanism for preserving <see cref="UE.Object"/> instances added to a <see cref="LogEvent"/>'s properties (e.g., via <see cref="LogContext"/>)
+/// so that the instance may be used as the <c>context</c> of a Unity <see cref="Debug.LogWarning(object, UE.Object)"/> message
+/// (without affecting the destructuring of other <see cref="UE.Object"/> log property values set by API consumers).
+/// </para>
+/// </summary>
+public sealed class UnityLogContextDestructuringPolicy : IDestructuringPolicy
+{
+    /// <inheritdoc />
+    public bool TryDestructure(object value, ILogEventPropertyValueFactory propertyValueFactory, [NotNullWhen(true)] out LogEventPropertyValue? result)
+    {
+        if (value is ValueTuple<UE.Object> context) {
+            result = new ScalarValue(context.Item1);
+            return true;
+        }
+
+        result = null;
+        return false;
+    }
+}
