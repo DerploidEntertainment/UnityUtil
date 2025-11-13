e635171b804d63267367896f7422f1b7496e936c
Dan Vicarel <dan.vicarel@gmail.com>
2025-11-13 12:14:02 -0500

    `ApplicationQuitter` now implements an interface for IoC

--- UnityUtil/Assets/UnityUtil/Runtime/ApplicationQuitter.cs
+++ UnityUtil/Assets/UnityUtil/Runtime/ApplicationQuitter.cs
@@ -5,15 +5,15 @@ using UnityEngine;
 namespace UnityUtil
 {
     [CreateAssetMenu(menuName = nameof(UnityUtil) + "/" + nameof(ApplicationQuitter), fileName = "application-quitter")]
-    public class ApplicationQuitter : ScriptableObject
+    public class ApplicationQuitter : ScriptableObject, IApplicationQuitter
     {
         [Button]
-        public void Quit()
+        public void Quit(int exitCode = 0)
         {
 #if UNITY_EDITOR
             EditorApplication.ExitPlaymode();
 #else
-            Application.Quit();
+            Application.Quit(exitCode);
 #endif
         }
 
---
+++ src/UnityUtil/UnityUtil/IApplicationQuitter.cs
@@ -0,0 +1,13 @@
+﻿namespace UnityUtil;
+
+/// <summary>
+/// Provides methods for quitting the entire application.
+/// </summary>
+public interface IApplicationQuitter
+{
+    /// <summary>
+    /// Quits the application with the specified exit code.
+    /// </summary>
+    /// <param name="exitCode"></param>
+    void Quit(int exitCode = 0);
+}
